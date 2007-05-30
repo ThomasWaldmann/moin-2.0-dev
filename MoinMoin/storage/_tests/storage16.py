ccb45a700435a00ea5f42728f17fefc3a4f63195
Heinrich Wendel <h_wendel@cojobo.net>
2007-05-31 01:00:21 +0200

    more user storage stuff

--- MoinMoin/storage/_tests/storage16.py
+++ MoinMoin/storage/_tests/storage16.py
@@ -10,43 +10,129 @@ import unittest
 
 from MoinMoin.storage.storage16 import UserStorage
 from MoinMoin.storage.error import NotImplementedError
+from MoinMoin.storage.error import StorageError
 
 class TestUserBackend(unittest.TestCase):
+    
+    datadir = "data/user"
+    
+    names = [ "1180352194.13.59241", "1180424607.34.55818", "1180424618.59.18110" ]
+    
+    metadata = {u'aliasname' : u'',
+                u'bookmarks' : {},
+                u'css_url' : u'',
+                u'date_fmt' : u'',
+                u'datetime_fmt' : u'',
+                u'disabled' : u'0',
+                u'edit_on_doubleclick' : u'0',
+                u'edit_rows' : u'20',
+                u'editor_default' : u'text',
+                u'editor_ui' : u'freechoice',
+                u'email' : u'h_wendel@cojobo.net',
+                u'enc_password' : u'{SHA}ujz/zJOpLgj4LDPFXYh2Zv3zZK4=',
+                u'language': u'',
+                u'last_saved': u'1180435816.61',
+                u'mailto_author' : u'0',
+                u'name': u'HeinrichWendel',
+                u'quicklinks': [u'www.google.de', u'www.test.de', u'WikiSandBox'],
+                u'remember_last_visit': u'0',
+                u'remember_me' : u'1',
+                u'show_comments' : u'0',
+                u'show_fancy_diff' : u'1',
+                u'show_nonexist_qm' : u'0',
+                u'show_page_trail' : u'1',
+                u'show_toolbar' : u'1',
+                u'show_topbottom' : u'0',
+                u'subscribed_pages' : [u'WikiSandBox', u'FrontPage2'],
+                u'theme_name' : u'modern',
+                u'tz_offset' : u'0',
+                u'want_trivial' : u'0',
+                u'wikiname_add_spaces' : u'0'
+                }
     
     backend = None
     
     def setUp(self):
-        self.backend = UserStorage()
+        self.backend = UserStorage("data/user/")
     
     def tearDown(self):
         self.backend = None
         
     def test_list_revisions(self):
-        self.assertEquals(self.backend.list_revisions("1180352194.13.59241"), [1])
+        self.assertEquals(self.backend.list_revisions(self.names[0]), [1])
         
     def test_current_revision(self):
-        self.assertEquals(self.backend.current_revision("1180352194.13.59241"), 1)
+        self.assertEquals(self.backend.current_revision(self.names[0]), 1)
         
     def test_create_revision(self):
         try:
-            self.backend.create_revision("1180352194.13.59241", 1)
+            self.backend.create_revision(self.names[0], 1)
             self.fail()
         except NotImplementedError:
             self.assertTrue(True)
     
     def test_remove_revision(self):
         try:
-            self.backend.remove_revision("1180352194.13.59241", 2)
+            self.backend.remove_revision(self.names[0], 2)
             self.fail()
         except NotImplementedError:
             self.assertTrue(True)
     
     def test_get_data_backend(self):
         try:
-            self.backend.get_data_backend("1180352194.13.59241", 1, "a")
+            self.backend.get_data_backend(self.names[0], 1, "a")
             self.fail()
         except NotImplementedError:
             self.assertTrue(True)
-            
+    
+    def test_list_items(self):
+        self.assertEquals(self.backend.list_items(), self.names)
+    
+    def test_has_item(self):
+        self.assertTrue(self.backend.has_item(self.names[0]))
+        self.assertFalse(self.backend.has_item("asdf"));
+  
+    def test_create_and_remove_item(self):
+        self.backend.create_item("test");
+        self.assertTrue(self.backend.has_item(self.names[0]))
+        
+        try:
+            self.backend.create_item(self.names[0]);
+            self.fail()
+        except StorageError:
+            self.assertTrue(True)
+
+        self.backend.remove_item("test");
+        self.assertFalse(self.backend.has_item("test"))
+        
+        try:
+            self.backend.remove_item("blub");
+            self.fail()
+        except StorageError:
+            self.assertTrue(True)
+    
+    def test_get_metadata(self):
+        self.__assertDicts(self.backend.get_metadata(self.names[0], 1), self.metadata)
+    
+    def test_set_metadata(self):
+        self.backend.set_metadata(self.names[0], 0, "aliasname", "test")
+        self.metadata["aliasname"] = "test";        
+        self.__assertDicts(self.backend.get_metadata(self.names[0], 1), self.metadata)
+        self.backend.set_metadata(self.names[0], 0, "aliasname", "")
+        self.metadata["aliasname"] = ""
+        self.__assertDicts(self.backend.get_metadata(self.names[0], 1), self.metadata)
+    
+    def test_remove_metadata(self):
+        self.backend.set_metadata(self.names[0], 0, "battle", "test")
+        self.metadata["battle"] = "test";        
+        self.__assertDicts(self.backend.get_metadata(self.names[0], 1), self.metadata)
+        self.backend.remove_metadata(self.names[0], 0, "battle")
+        del self.metadata["battle"]        
+        self.__assertDicts(self.backend.get_metadata(self.names[0], 1), self.metadata)
+    
+    def __assertDicts(self, dict1, dict2):
+        for key, value in dict1.iteritems():
+            self.assertEquals(dict2[key], value)
+    
 if __name__ == "__main__":
         unittest.main()
--- MoinMoin/storage/storage16.py
+++ MoinMoin/storage/storage16.py
@@ -6,36 +6,60 @@
 """
 
 from MoinMoin.storage.interfaces import StorageBackend
+from MoinMoin.storage.error import StorageError
+from MoinMoin import config
+
+import os.path
+import os
+import codecs
 
 class UserStorage(StorageBackend):
     """
     Class that implements the 1.6 compatible storage backend for users.
     """
     
+    path = None
+    
+    def __init__(self, path):
+        """
+        Init the Backend with the correct path.
+        """
+        if not os.path.isdir(path):
+            raise StorageError("Invalid path '%s'." % path)
+        self.path = path
+    
     def list_items(self, filters=None):
         """ 
         @see MoinMoin.interfaces.StorageBackend.list_items
         """
-        raise NotImplementedError
+        # TODO: filters
+        return os.listdir(self.path)
 
     def has_item(self, name):
         """
         @see MoinMoin.interfaces.StorageBackend.has_item
         """
-        raise NotImplementedError
+        return os.path.isfile(os.path.join(self.path, name))
 
     def create_item(self, name):
         """
         @see MoinMoin.interfaces.StorageBackend.create_item
         """
-        raise NotImplementedError
+        if not self.has_item(name):
+            fd = open(os.path.join(self.path, name), "w")
+            fd.close()
+        else:
+            raise StorageError("Item '%s' already exists" % name)
 
     def remove_item(self, name):
         """
         @see MoinMoin.interfaces.StorageBackend.remove_item
         """
-        raise NotImplementedError
-
+        if self.has_item(name):
+            fd = os.remove(os.path.join(self.path, name))
+        else:
+            raise StorageError("Item '%s' does not exist" % name)
+        
     def list_revisions(self, name):
         """
         @see MoinMoin.interfaces.StorageBackend.list_revisions
@@ -56,16 +80,130 @@ class UserStorage(StorageBackend):
         """
         @see MoinMoin.interfaces.StorageBackend.get_metadata
         """
-        raise NotImplementedError
+        return self.__parseMetadata(name)
 
     def set_metadata(self, name, revno, key, value):
         """
         @see MoinMoin.interfaces.StorageBackend.get_data_backend
         """
-        raise NotImplementedError
+        metadata = self.__parseMetadata(name)
+        metadata[key] = value
+        self.__saveMetadata(name, metadata)
 
     def remove_metadata(self, name, revno, key):
         """
         @see MoinMoin.interfaces.StorageBackend.get_data_backend
         """
-        raise NotImplementedError
+        metadata = self.__parseMetadata(name)
+        del metadata[key]
+        self.__saveMetadata(name, metadata)
+
+    def __parseMetadata(self, name):
+        """
+        Read the metadata fromt the file.
+        """
+        data = codecs.open(os.path.join(self.path, name), "r", config.charset).readlines()
+        user_data = {}
+        for line in data:
+            if line[0] == '#':
+                continue
+
+            try:
+                key, val = line.strip().split('=', 1)
+                # Decode list values
+                if key.endswith('[]'):
+                    key = key[:-2]
+                    val = decodeList(val)
+                # Decode dict values
+                elif key.endswith('{}'):
+                    key = key[:-2]
+                    val = decodeDict(val)
+                user_data[key] = val
+            except ValueError:
+                pass
+        return user_data
+    
+    def __saveMetadata(self, name, metadata):
+        """
+        Save the data to the file.
+        """
+        data = codecs.open(os.path.join(self.path, name), "w", config.charset)
+        for key, value in metadata.iteritems():
+            # Encode list values
+            if isinstance(value, list):
+                key += '[]'
+                value = encodeList(value)
+            # Encode dict values
+            elif isinstance(value, dict):
+                key += '{}'
+                value = encodeDict(value)
+            line = u"%s=%s\n" % (key, unicode(value))
+            data.write(line)
+        data.close()
+
+def encodeList(items):
+    """ Encode list of items in user data file
+
+    Items are separated by '\t' characters.
+    
+    @param items: list unicode strings
+    @rtype: unicode
+    @return: list encoded as unicode
+    """
+    line = []
+    for item in items:
+        item = item.strip()
+        if not item:
+            continue
+        line.append(item)
+
+    line = '\t'.join(line)
+    return line
+
+def decodeList(line):
+    """ Decode list of items from user data file
+    
+    @param line: line containing list of items, encoded with encodeList
+    @rtype: list of unicode strings
+    @return: list of items in encoded in line
+    """
+    items = []
+    for item in line.split('\t'):
+        item = item.strip()
+        if not item:
+            continue
+        items.append(item)
+    return items
+
+def encodeDict(items):
+    """ Encode dict of items in user data file
+
+    Items are separated by '\t' characters.
+    Each item is key:value.
+    
+    @param items: dict of unicode:unicode
+    @rtype: unicode
+    @return: dict encoded as unicode
+    """
+    line = []
+    for key, value in items.items():
+        item = u'%s:%s' % (key, value)
+        line.append(item)
+    line = '\t'.join(line)
+    return line
+
+def decodeDict(line):
+    """ Decode dict of key:value pairs from user data file
+    
+    @param line: line containing a dict, encoded with encodeDict
+    @rtype: dict
+    @return: dict  unicode:unicode items
+    """
+    items = {}
+    for item in line.split('\t'):
+        item = item.strip()
+        if not item:
+            continue        
+        key, value = item.split(':', 1)
+        items[key] = value
+    return items
