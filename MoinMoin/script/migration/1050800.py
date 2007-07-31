ccfdbb5634a4d8cf83440eeaa0589e4c644811e3
TW <tw@waldmann-edv.de>
2007-08-01 00:46:59 +0200

    1.5.x to 1.6 conversion (unfinished)

---
+++ MoinMoin/script/migration/1050800.py
@@ -0,0 +1,16 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - 1st pass of 1.6 migration
+
+    @copyright: 2007 by Thomas Waldmann
+    @license: GNU GPL, see COPYING for details.
+"""
+
+from conv160 import DataConverter
+
+def execute(script, data_dir, rev):
+    # the first pass just creates <data_dir>/rename1.txt
+    dc = DataConverter(data_dir, None)
+    dc.pass1()
+    return 1059999
+
