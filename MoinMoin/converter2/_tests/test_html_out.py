b0225a449bc4dd4ed91ec0394bad8d698a1aa61c
Bastian Blank <bblank@thinkmo.de>
2008-06-04 18:54:16 +0200

    Initial converter.
    * MoinMoin/converter2/_tests/test_html_out.py
      - Add doc.
      - Add testcase.
    * MoinMoin/converter2/html_out.py
      - Update doc.
      - Add recursion.

--- MoinMoin/converter2/_tests/test_html_out.py
+++ MoinMoin/converter2/_tests/test_html_out.py
@@ -1,1 +1,21 @@
+"""
+MoinMoin - Tests for MoinMoin.converter2.html_out
+
+@copyright: 2007 MoinMoin:BastianBlank
+@license: GNU GPL, see COPYING for details.
+"""
+
+import py.test
+
 from MoinMoin.converter2.html_out import *
+
+namespaces_string = 'xmlns:page="%s" xmlns:html="%s"' % (namespaces.moin_page, namespaces.html)
+
+class TestConverterBase(object):
+    def setup_class(self):
+        self.conv = ConverterBase()
+
+    def test_unknown(self):
+        page = ElementTree.XML("<page:unknown %s/>" % namespaces_string)
+        py.test.raises(Exception, self.conv.recurse, page)
+
--- MoinMoin/converter2/html_out.py
+++ MoinMoin/converter2/html_out.py
@@ -1,12 +1,34 @@
 """
+MoinMoin - HTML output converter
+
 @copyright: 2008 MoinMoin:BastianBlank
 @license: GNU GPL, see COPYING for details.
 """
 
+from emeraldtree import ElementTree
+
 from MoinMoin.util import namespaces
 
 class ConverterBase(object):
-    pass
+    def recurse(self, element):
+        new = []
+        for child in element:
+            if isinstance(child, ElementTree.Element):
+                r = self.visit(child)
+            else:
+                r = child
+            if r is not None:
+                new.append(r)
+        element[:] = new
+
+    def visit(self, elem):
+        if elem.tag.uri == namespaces.moin_page:
+            return self.visit_moinpage(elem)
+        # TODO
+        raise Exception
+
+    def visit_moinpage(self, elem):
+        return elem
 
 class Converter(ConverterBase):
     """
