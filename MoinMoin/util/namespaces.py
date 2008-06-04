43b805e6e1f176c6c4c2ed6c630eef7674edc6d6
bastian <bastian@shockwave.waldi.eu.org>
2008-06-04 11:19:58 +0200

    Add definitions of own and well known (xml) namespaces.
    MoinMoin/util/namespaces.py
    - Add namespace "http://moinmo.in/namespaces/page".
    - Add well known namespaces dc, html and xlink.

---
+++ MoinMoin/util/namespaces.py
@@ -0,0 +1,11 @@
+"""
+@copyright: 2008 MoinMoin:BastianBlank
+@license: GNU GPL, see COPYING for details.
+"""
+
+moin_page = 'http://moinmo.in/namespaces/page'
+
+dc = 'http://purl.org/dc/elements/1.1/'
+html = 'http://www.w3.org/1999/xhtml'
+xlink = 'http://www.w3.org/1999/xlink'
+
