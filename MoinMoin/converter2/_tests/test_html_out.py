f577ba147c611210d5bd38189895d42df9e46cef
Bastian Blank <bblank@thinkmo.de>
2008-06-08 12:48:40 +0200

    Add real testcases. Fix exception.
    MoinMoin/converter2/_tests/test_html_out.py
    - Add basic testcase for sucessfull conversion.
    - Check correct exception.

--- MoinMoin/converter2/_tests/test_html_out.py
+++ MoinMoin/converter2/_tests/test_html_out.py
@@ -11,11 +11,31 @@ from MoinMoin.converter2.html_out import *
 
 namespaces_string = 'xmlns:page="%s" xmlns:html="%s"' % (namespaces.moin_page, namespaces.html)
 
+def serialize(elem, **options):
+    from cStringIO import StringIO
+    file = StringIO()
+    tree = ElementTree.ElementTree(elem)
+    tree.write(file, **options)
+    return file.getvalue()
+
 class TestConverterBase(object):
     def setup_class(self):
         self.conv = ConverterBase()
 
+    def test_base(self):
+        pairs = [
+            ('<page:page %s><page:p>Test</page:p></page:page>',
+                '<html:div xmlns:html="http://www.w3.org/1999/xhtml"><html:p>Test</html:p></html:div>'),
+        ]
+        for i in pairs:
+            yield (self._do_base,) + i
+
+    def _do_base(self, input, output):
+        page = ElementTree.XML(input % namespaces_string)
+        out = self.conv(page)
+        assert serialize(out) == output
+
     def test_unknown(self):
         page = ElementTree.XML("<page:unknown %s/>" % namespaces_string)
-        py.test.raises(Exception, self.conv.recurse, page)
+        py.test.raises(ElementException, self.conv.__call__, page)
 
