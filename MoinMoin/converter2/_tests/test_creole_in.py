072bd43466e8214f3c36a9dbdd36d09c72702f90
Bastian Blank <bblank@thinkmo.de>
2008-06-23 09:52:50 +0200

    Add Creole input converter.
    * MoinMoin/converter2/creole_in.py: Copy from MoinMoin/parser/_creole.py.
    * MoinMoin/converter2/_tests/test_creole_in.py: New file.

---
+++ MoinMoin/converter2/_tests/test_creole_in.py
@@ -0,0 +1,38 @@
+"""
+MoinMoin - Tests for MoinMoin.converter2.creole_in
+
+@copyright: 2008 MoinMoin:BastianBlank
+@license: GNU GPL, see COPYING for details.
+"""
+
+import py.test
+
+from MoinMoin.converter2.creole_in import *
+
+namespaces_string = 'xmlns="%s"' % namespaces.moin_page
+
+def serialize(elem, **options):
+    from cStringIO import StringIO
+    file = StringIO()
+    tree = ElementTree.ElementTree(elem)
+    tree.write(file, default_namespace = namespaces.moin_page, **options)
+    return file.getvalue()
+
+class TestConverter(object):
+    def setup_class(self):
+        self.conv = Converter()
+
+    def test_base(self):
+        pairs = [
+            ('= Heading 1',
+                '<page %s><h outline-level="1">Heading 1</h></page>' % namespaces_string),
+            ('== Heading 2',
+                '<page %s><h outline-level="2">Heading 2</h></page>' % namespaces_string),
+        ]
+        for i in pairs:
+            yield (self._do,) + i
+
+    def _do(self, input, output):
+        out = self.conv(input)
+        assert serialize(out) == output
+
