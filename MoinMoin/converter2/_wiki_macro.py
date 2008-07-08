230e7c53b40695b54f69dd14b7a7f474c7c9f559
Bastian Blank <bblank@thinkmo.de>
2008-07-08 14:26:52 +0200

    Pseudo-macro handling - Wrap into block if necessary

--- MoinMoin/converter2/_wiki_macro.py
+++ MoinMoin/converter2/_wiki_macro.py
@@ -12,13 +12,15 @@ from MoinMoin.util import namespaces
 
 class ConverterMacro(object):
     def _BR_repl(self, args, text, type):
-        return ElementTree.Element(ElementTree.QName('line-break', namespaces.moin_page))
+        elem = ElementTree.Element(ElementTree.QName('line-break', namespaces.moin_page))
+        return self.wrap_block(type, elem)
 
     def _FootNote_repl(self, args, text, type):
         tag = ElementTree.QName('note', namespaces.moin_page)
         tag_body = ElementTree.QName('note-body', namespaces.moin_page)
         tag_class = ElementTree.QName('note-class', namespaces.moin_page)
-        return ElementTree.Element(tag, attrib={tag_class: 'footnote'}, children=[text])
+        elem = ElementTree.Element(tag, attrib={tag_class: 'footnote'}, children=[text])
+        return self.wrap_block(type, elem)
 
     def macro(self, name, args, text, type):
         func = getattr(self, '_%s_repl' % name, None)
@@ -33,3 +35,9 @@ class ConverterMacro(object):
         tag_alt = ElementTree.QName('alt', namespaces.moin_page)
         attrib = {tag_name: name, tag_args: args, tag_type: type, tag_alt: text}
         return ElementTree.Element(tag, attrib)
+
+    def wrap_block(self, type, elem):
+        if type == 'block':
+            tag = ElementTree.QName('p', namespaces.moin_page)
+            return ElementTree.Element(tag, children=[elem])
+        return elem
