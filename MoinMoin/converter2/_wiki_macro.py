c03a85c6b363f1e0b5d94740e0eb4374e0cbf336
Bastian Blank <bblank@thinkmo.de>
2008-07-13 16:35:43 +0200

    Pseudo-macro handling - Use import as to reduce line lenght

--- MoinMoin/converter2/_wiki_macro.py
+++ MoinMoin/converter2/_wiki_macro.py
@@ -6,20 +6,20 @@ MoinMoin - Macro and pseudo-macro handling
 @license: GNU GPL, see COPYING for details.
 """
 
-from emeraldtree import ElementTree
+from emeraldtree import ElementTree as ET
 
 from MoinMoin.util import namespaces
 
 class ConverterMacro(object):
     def _BR_repl(self, args, text, type):
-        elem = ElementTree.Element(ElementTree.QName('line-break', namespaces.moin_page))
+        elem = ET.Element(ET.QName('line-break', namespaces.moin_page))
         return self.wrap_block(type, elem)
 
     def _FootNote_repl(self, args, text, type):
-        tag = ElementTree.QName('note', namespaces.moin_page)
-        tag_body = ElementTree.QName('note-body', namespaces.moin_page)
-        tag_class = ElementTree.QName('note-class', namespaces.moin_page)
-        elem = ElementTree.Element(tag, attrib={tag_class: 'footnote'}, children=[text])
+        tag = ET.QName('note', namespaces.moin_page)
+        tag_body = ET.QName('note-body', namespaces.moin_page)
+        tag_class = ET.QName('note-class', namespaces.moin_page)
+        elem = ET.Element(tag, attrib={tag_class: 'footnote'}, children=[text])
         return self.wrap_block(type, elem)
 
     def macro(self, name, args, text, type):
@@ -28,16 +28,16 @@ class ConverterMacro(object):
             return func(args, text, type)
 
         # TODO: other namespace?
-        tag = ElementTree.QName('macro', namespaces.moin_page)
-        tag_name = ElementTree.QName('macro-name', namespaces.moin_page)
-        tag_args = ElementTree.QName('macro-args', namespaces.moin_page)
-        tag_type = ElementTree.QName('macro-type', namespaces.moin_page)
-        tag_alt = ElementTree.QName('alt', namespaces.moin_page)
+        tag = ET.QName('macro', namespaces.moin_page)
+        tag_name = ET.QName('macro-name', namespaces.moin_page)
+        tag_args = ET.QName('macro-args', namespaces.moin_page)
+        tag_type = ET.QName('macro-type', namespaces.moin_page)
+        tag_alt = ET.QName('alt', namespaces.moin_page)
         attrib = {tag_name: name, tag_args: args, tag_type: type, tag_alt: text}
-        return ElementTree.Element(tag, attrib)
+        return ET.Element(tag, attrib)
 
     def wrap_block(self, type, elem):
         if type == 'block':
-            tag = ElementTree.QName('p', namespaces.moin_page)
-            return ElementTree.Element(tag, children=[elem])
+            tag = ET.QName('p', namespaces.moin_page)
+            return ET.Element(tag, children=[elem])
         return elem
