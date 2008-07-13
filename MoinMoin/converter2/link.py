2a1b10fa4f52a365422a00935433ca0b0f4d2f46
Bastian Blank <bblank@thinkmo.de>
2008-07-13 15:40:52 +0200

    Link converter - Use page base url

--- MoinMoin/converter2/link.py
+++ MoinMoin/converter2/link.py
@@ -5,19 +5,22 @@ MoinMoin - Link converter
 @license: GNU GPL, see COPYING for details.
 """
 
-from emeraldtree import ElementTree
+from emeraldtree import ElementTree as ET
 
 from MoinMoin import wikiutil
 from MoinMoin.util import namespaces
 
 class ConverterExternOutput(object):
+    tag_href = ET.QName('href', namespaces.xlink)
+    tag_page_href = ET.QName('page-href', namespaces.moin_page)
+
     @classmethod
     def _factory(cls, input, output):
         if input == 'application/x-moin-document' and \
                 output == 'application/x-moin-document;links=extern':
             return cls()
 
-    def handle_wikilocal(self, link):
+    def handle_wikilocal(self, link, page_name):
         if ':' in link:
             wiki_name, link = link.split(':', 1)
 
@@ -37,12 +40,11 @@ class ConverterExternOutput(object):
         except ValueError:
             anchor = None
 
-        current_page = self.page.page_name
         if not link:
-            link = current_page
+            link = page_name
 
         # handle relative links
-        link = wikiutil.AbsPageName(current_page, link)
+        link = wikiutil.AbsPageName(page_name, link)
 
         if anchor:
             link = link + '#' + wikiutil.url_quote_plus(anchor)
@@ -50,17 +52,30 @@ class ConverterExternOutput(object):
         # TODO query string
         return self.request.getScriptname() + '/' + link
 
-    def __call__(self, tree, request, page):
-        self.request, self.page = request, page
+    def recurse(self, elem, page_href):
+        page_href = elem.get(self.tag_page_href, page_href)
 
-        tag = ElementTree.QName('href', namespaces.xlink)
-        for elem in tree.iter():
-            href = elem.get(tag, None)
-            if href is not None:
-                if href.startswith('wiki.local:'):
-                    href = self.handle_wikilocal(href[11:])
-                    if href is not None:
-                        elem.set(tag, href)
+        href = elem.get(self.tag_href, None)
+        if href is not None:
+            yield elem, href, page_href
+
+        for child in elem:
+            if isinstance(child, ET.Node):
+                for i in self.recurse(child, page_href):
+                    yield i
+
+    def __call__(self, tree, request):
+        self.request = request
+
+        for elem, href, page_href in self.recurse(tree, None):
+            if href.startswith('wiki.local:'):
+                if page_href.startswith('wiki:///'):
+                    page_name = page_href[8:]
+                else:
+                    page_name = ''
+                href = self.handle_wikilocal(href[11:], page_name)
+                if href is not None:
+                    elem.set(self.tag_href, href)
         return tree
 
 from _registry import default_registry
