e2700de7a3edd40ceb75fb2f8af19141d90c6840
Bastian Blank <bblank@thinkmo.de>
2008-07-25 19:00:07 +0200

    Include converter - Add XPointer parser

--- MoinMoin/converter2/include.py
+++ MoinMoin/converter2/include.py
@@ -8,10 +8,67 @@ Expands include elements in a internal Moin document.
 """
 
 from emeraldtree import ElementTree as ET
+import re
 
 from MoinMoin import wikiutil
 from MoinMoin.Page import Page
 from MoinMoin.util import namespaces
+
+class XPointer(list):
+    tokenizer_rules = r"""
+        \^[()^]
+        |
+        (?P<braket_open> \( )
+        |
+        (?P<braket_close> \) )
+        |
+        (?P<whitespace> \s+ )
+        |
+        [^()^]+
+    """
+    tokenizer_re = re.compile(tokenizer_rules, re.X)
+
+    class Entry(object):
+        __slots__ = 'name', 'data'
+
+        def __init__(self, name, data):
+            self.name, self.data = name, data
+
+        @property
+        def data_unescape(self):
+            data = self.data.replace('^(', '(').replace('^)', ')')
+            return data.replace('^^', '^')
+
+    def __init__(self, input):
+        name = []
+        stack = []
+
+        for match in self.tokenizer_re.finditer(input):
+            if match.group('braket_open'):
+                stack.append([])
+            elif match.group('braket_close'):
+                top = stack.pop()
+                if stack:
+                    stack[-1].extend(top)
+                else:
+                    self.append(self.Entry(''.join(name), ''.join(top)))
+                    name = []
+            else:
+                if stack:
+                    stack[-1].append(match.group())
+                elif not match.group('whitespace'):
+                    name.append(match.group())
+
+        while len(stack) > 1:
+            top = stack.pop()
+            stack[-1].extend(top)
+
+        if name:
+            if stack:
+                data = ''.join(stack.pop())
+            else:
+                data = None
+            self.append(self.Entry(''.join(name), None))
 
 class Converter(object):
     tag_div = ET.QName('div', namespaces.moin_page)
@@ -46,18 +103,20 @@ class Converter(object):
                     elif href.startswith('wiki.local:'):
                         include = wikiutil.AbsPageName(page_href[8:], href[11:])
 
-                    doc = Page(self.request, include).convert_input_cache(self.request)
-                    pages = ((doc, 'wiki:///' + include),)
+                    page = Page(self.request, include)
+                    pages = ((page, 'wiki:///' + include), )
                 else:
                     raise NotImplementedError
 
                 div = ET.Element(self.tag_div)
 
-                for page_doc, page_href in pages:
+                for page, page_href in pages:
                     if page_href in self.stack:
                         # TODO: Found cycle, warn
                         continue
+                    # TODO: Check permissions
 
+                    page_doc = page.convert_input_cache(self.request)
                     page_doc.tag = self.tag_div
                     self.recurse(page_doc, page_href)
                     div.append(page_doc)
