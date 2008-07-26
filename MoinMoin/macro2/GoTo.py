71f6652e9a0c72b6c27b495fea9d0656322dfc45
Bastian Blank <bblank@thinkmo.de>
2008-07-26 13:26:43 +0200

    GoTo macro - Use ElementTree.XML

--- MoinMoin/macro2/GoTo.py
+++ MoinMoin/macro2/GoTo.py
@@ -16,24 +16,13 @@ class Macro(MacroBlockBase):
     def macro(self):
         _ = self.request.getText
 
-        tag_form = ET.QName('form', namespaces.html)
-        tag_input = ET.QName('input', namespaces.html)
-        tag_name = ET.QName('name', namespaces.html)
-        tag_p = ET.QName('p', namespaces.html)
-        tag_size = ET.QName('size', namespaces.html)
-        tag_type = ET.QName('type', namespaces.html)
-        tag_value = ET.QName('value', namespaces.html)
-
-        attrib = {tag_type: 'text', tag_name: 'target', tag_size: '30'}
-        input1 = ET.Element(tag_input, attrib=attrib)
-        attrib = {tag_type: 'submit', tag_value: _("Go To Page")}
-        input2 = ET.Element(tag_input, attrib=attrib)
-
-        p = ET.Element(tag_p, children=[input1, ' ', input2])
-
-        attrib = {tag_type: 'hidden', tag_name: 'action', tag_value: 'goto'}
-        input = ET.Element(tag_input, attrib=attrib)
-        form = ET.Element(tag_form, children=[input, p])
-
-        return form
+        return ET.XML("""
+<form xmlns="%s">
+    <input type="hidden" name="action" value="goto" />
+    <p>
+        <input type="text" name="target" size="30" />
+        <input type="submit" value="%s" />
+    </p>
+</form>
+""" % (namespaces.html, _("Go To Page")))
 
