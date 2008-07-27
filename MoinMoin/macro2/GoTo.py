f6753a2f446b7da3b87259c30e43264bea00bbd2
Bastian Blank <bblank@thinkmo.de>
2008-07-27 16:14:21 +0200

    GoTo macro - Readd method and action attributes

--- MoinMoin/macro2/GoTo.py
+++ MoinMoin/macro2/GoTo.py
@@ -17,12 +17,15 @@ class Macro(MacroBlockBase):
         _ = self.request.getText
 
         return ET.XML("""
-<form xmlns="%s">
+<form xmlns="%s" method="get" action="%s/%s">
     <input type="hidden" name="action" value="goto" />
     <p>
         <input type="text" name="target" size="30" />
         <input type="submit" value="%s" />
     </p>
 </form>
-""" % (namespaces.html, _("Go To Page")))
+""" % (namespaces.html,
+        self.request.getScriptname(),
+        self.page_name,
+        _("Go To Page")))
 
