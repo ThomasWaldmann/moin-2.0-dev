afb235f06fe11d958a9acf6ccbae94628dec960e
Bastian Blank <bblank@thinkmo.de>
2008-07-29 22:13:17 +0200

    Moin Wiki input converter - Redo interwiki support

--- MoinMoin/converter2/moinwiki_in.py
+++ MoinMoin/converter2/moinwiki_in.py
@@ -358,34 +358,43 @@ class Converter(ConverterMacro):
         self._stack_top_append(element)
 
     inline_freelink = r"""
-        (?:
-         (?<![%(u)s%(l)s/])  # require anything not upper/lower/slash before
-         |
-         ^  # ... or beginning of line
-        )
-        (?P<freelink_bang>\!)?  # configurable: avoid getting CamelCase rendered as link
         (?P<freelink>
-         ([A-Z][a-zA-Z]+:)?
-         (?P<freelink_page>
-          (?:
-           (%(parent)s)*  # there might be either ../ parent prefix(es)
-           |
-           ((?<!%(child)s)%(child)s)?  # or maybe a single / child prefix (but not if we already had it before)
+         (?:
+          (?<![%(u)s%(l)s/])  # require anything not upper/lower/slash before
+          |
+          ^  # ... or beginning of line
+         )
+         (?P<freelink_bang>\!)?  # configurable: avoid getting CamelCase rendered as link
+         (
+          (?P<freelink_interwiki>
+           [A-Z][a-zA-Z]+\:
+           (?P<freelink_interwiki_page>
+            (?=[^ ]*[%(u)s%(l)s0..9][^ ]* )  # make sure there is something non-blank with at least one alphanum letter following
+            [^"\'}\]|:,.\)?!]+  # we take all until we hit some blank or punctuation char ...
+           )
           )
-          (
-           ((?<!%(child)s)%(child)s)?  # there might be / child prefix (but not if we already had it before)
-           (?:[%(u)s][%(l)s]+){2,}  # at least 2 upper>lower transitions make CamelCase
-          )+  # we can have MainPage/SubPage/SubSubPage ...
+          |
+          (?P<freelink_page>
+           (?:
+            (%(parent)s)*  # there might be either ../ parent prefix(es)
+            |
+            ((?<!%(child)s)%(child)s)?  # or maybe a single / child prefix (but not if we already had it before)
+           )
+           (
+            ((?<!%(child)s)%(child)s)?  # there might be / child prefix (but not if we already had it before)
+            (?:[%(u)s][%(l)s]+){2,}  # at least 2 upper>lower transitions make CamelCase
+           )+  # we can have MainPage/SubPage/SubSubPage ...
+           (?:
+            \#  # anchor separator          TODO check if this does not make trouble at places where word_rule is used
+            \S+  # some anchor name
+           )?
+          )
          )
          (?:
-          \#  # anchor separator          TODO check if this does not make trouble at places where word_rule is used
-          \S+  # some anchor name
-         )?
-        )
-        (?:
-         (?![%(u)s%(l)s/])  # require anything not upper/lower/slash following
-         |
-         $  # ... or end of line
+          (?![%(u)s%(l)s/])  # require anything not upper/lower/slash following
+          |
+          $  # ... or end of line
+         )
         )
     """ % {
         'u': config.chars_upper,
@@ -394,16 +403,21 @@ class Converter(ConverterMacro):
         'parent': re.escape(wikiutil.PARENT_PREFIX),
     }
 
-    def inline_freelink_repl(self, freelink, freelink_page, freelink_bang=None):
+    def inline_freelink_repl(self, freelink, freelink_bang=None,
+            freelink_interwiki=None, freelink_interwiki_page=None,
+            freelink_page=None):
+        link = freelink_page or freelink_interwiki
+        text = freelink_page or freelink_interwiki_page
+
         if freelink_bang:
-            self._stack_top_append(freelink)
+            self._stack_top_append(text)
             return
 
         tag = ET.QName('a', namespaces.moin_page)
         tag_href = ET.QName('href', namespaces.xlink)
 
-        attrib = {tag_href: 'wiki.local:' + freelink}
-        element = ET.Element(tag, attrib, children=[freelink_page])
+        attrib = {tag_href: 'wiki.local:' + link}
+        element = ET.Element(tag, attrib, children=[text])
         self._stack_top_append(element)
 
     # Block elements
