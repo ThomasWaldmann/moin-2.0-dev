53f8748e14166336a2ce4f97a1966502d2d02ae6
Bastian Blank <bblank@thinkmo.de>
2008-07-29 18:29:22 +0200

    Test for HTML output converter - Cleanup namespace usage

--- MoinMoin/converter2/_tests/test_html_out.py
+++ MoinMoin/converter2/_tests/test_html_out.py
@@ -9,13 +9,14 @@ import py.test
 
 from MoinMoin.converter2.html_out import *
 
-ns_str_default = 'xmlns="%s" xmlns:html="%s" xmlns:xlink="%s"' % (
-        namespaces.moin_page, namespaces.html, namespaces.xlink)
-namespaces_string_html = 'xmlns:html="%s"' % namespaces.html
-namespaces_string_html_default = 'xmlns="%s"' % namespaces.html
-namespaces_string_page = 'xmlns:page="%s"' % namespaces.moin_page
-namespaces_string_page_default = 'xmlns="%s"' % namespaces.moin_page
-namespaces_string_xlink = 'xmlns:xlink="%s"' % namespaces.xlink
+ns_all = 'xmlns="%s" xmlns:page="%s" xmlns:html="%s" xmlns:xlink="%s"' % (
+        namespaces.moin_page,
+        namespaces.moin_page,
+        namespaces.html,
+        namespaces.xlink)
+ns_html = 'xmlns="%s"' % namespaces.html
+ns_html_real = 'xmlns:html="%s"' % namespaces.html
+ns_page = 'xmlns:page="%s"' % namespaces.moin_page
 
 def serialize(elem, **options):
     from cStringIO import StringIO
@@ -31,83 +32,82 @@ class TestConverterBase(object):
 
     def test_base(self):
         pairs = [
-            ('<page:page %s><page:p>Test</page:p></page:page>' % namespaces_string_page,
-                '<div %s><p>Test</p></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:h>Test</page:h></page:page>' % namespaces_string_page,
-                '<div %s><h1>Test</h1></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:h page:outline-level="2">Test</page:h></page:page>' % namespaces_string_page,
-                '<div %s><h2>Test</h2></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:h page:outline-level="6">Test</page:h></page:page>' % namespaces_string_page,
-                '<div %s><h6>Test</h6></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:h page:outline-level="7">Test</page:h></page:page>' % namespaces_string_page,
-                '<div %s><h6>Test</h6></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:a xlink:href="uri:test">Test</page:a></page:page>' %
-                ' '.join([namespaces_string_page, namespaces_string_xlink]),
-                '<div %s><a href="uri:test">Test</a></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:p>Test<page:line-break/>Test</page:p></page:page>' % namespaces_string_page,
-                '<div %s><p>Test<br />Test</p></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:p>Test<page:span>Test</page:span></page:p></page:page>' % namespaces_string_page,
-                '<div %s><p>Test<span>Test</span></p></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:p><page:emphasis>Test</page:emphasis></page:p></page:page>' % namespaces_string_page,
-                '<div %s><p><em>Test</em></p></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:p><page:strong>Test</page:strong></page:p></page:page>' % namespaces_string_page,
-                '<div %s><p><strong>Test</strong></p></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:blockcode>Code</page:blockcode></page:page>' % namespaces_string_page,
-                '<div %s><pre>Code</pre></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:p><page:code>Code</page:code></page:p></page:page>' % namespaces_string_page,
-                '<div %s><p><tt>Code</tt></p></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:separator/></page:page>' % namespaces_string_page,
-                '<div %s><hr /></div>' % namespaces_string_html_default),
-            ('<page:page %s><page:div><page:p>Text</page:p></page:div></page:page>' % namespaces_string_page,
-                '<div %s><div><p>Text</p></div></div>' % namespaces_string_html_default),
+            ('<page:page %s><page:p>Test</page:p></page:page>' % ns_all,
+                '<div %s><p>Test</p></div>' % ns_html),
+            ('<page:page %s><page:h>Test</page:h></page:page>' % ns_all,
+                '<div %s><h1>Test</h1></div>' % ns_html),
+            ('<page:page %s><page:h page:outline-level="2">Test</page:h></page:page>' % ns_all,
+                '<div %s><h2>Test</h2></div>' % ns_html),
+            ('<page:page %s><page:h page:outline-level="6">Test</page:h></page:page>' % ns_all,
+                '<div %s><h6>Test</h6></div>' % ns_html),
+            ('<page:page %s><page:h page:outline-level="7">Test</page:h></page:page>' % ns_all,
+                '<div %s><h6>Test</h6></div>' % ns_html),
+            ('<page:page %s><page:a xlink:href="uri:test">Test</page:a></page:page>' % ns_all,
+                '<div %s><a href="uri:test">Test</a></div>' % ns_html),
+            ('<page:page %s><page:p>Test<page:line-break/>Test</page:p></page:page>' % ns_all,
+                '<div %s><p>Test<br />Test</p></div>' % ns_html),
+            ('<page:page %s><page:p>Test<page:span>Test</page:span></page:p></page:page>' % ns_all,
+                '<div %s><p>Test<span>Test</span></p></div>' % ns_html),
+            ('<page:page %s><page:p><page:emphasis>Test</page:emphasis></page:p></page:page>' % ns_all,
+                '<div %s><p><em>Test</em></p></div>' % ns_html),
+            ('<page:page %s><page:p><page:strong>Test</page:strong></page:p></page:page>' % ns_all,
+                '<div %s><p><strong>Test</strong></p></div>' % ns_html),
+            ('<page:page %s><page:blockcode>Code</page:blockcode></page:page>' % ns_all,
+                '<div %s><pre>Code</pre></div>' % ns_html),
+            ('<page:page %s><page:p><page:code>Code</page:code></page:p></page:page>' % ns_all,
+                '<div %s><p><tt>Code</tt></p></div>' % ns_html),
+            ('<page:page %s><page:separator/></page:page>' % ns_all,
+                '<div %s><hr /></div>' % ns_html),
+            ('<page:page %s><page:div><page:p>Text</page:p></page:div></page:page>' % ns_all,
+                '<div %s><div><p>Text</p></div></div>' % ns_html),
         ]
         for i in pairs:
             yield (self._do, ) + i
 
     def test_html(self):
         pairs = [
-            ('<html:div html:id="a" id="b" %s><html:p id="c">Test</html:p></html:div>' % namespaces_string_html,
-                '<div id="a" %s><p id="c">Test</p></div>' % namespaces_string_html_default),
+            ('<html:div html:id="a" id="b" %s><html:p id="c">Test</html:p></html:div>' % ns_all,
+                '<div id="a" %s><p id="c">Test</p></div>' % ns_html),
         ]
         for i in pairs:
             yield (self._do, ) + i
 
     def test_list(self):
         pairs = [
-            ('<page %s><list item-label-generate="unordered"><list-item><list-item-body>Item</list-item-body></list-item></list></page>' % namespaces_string_page_default,
-                '<div %s><ul><li>Item</li></ul></div>' % namespaces_string_html_default),
-            ('<page %s><list item-label-generate="ordered"><list-item><list-item-body>Item</list-item-body></list-item></list></page>' % namespaces_string_page_default,
-                '<div %s><ol><li>Item</li></ol></div>' % namespaces_string_html_default),
-            ('<page %s><list><list-item><list-item-label>Label</list-item-label><list-item-body>Item</list-item-body></list-item></list></page>' % namespaces_string_page_default,
-                '<div %s><dl><dt>Label</dt><dd>Item</dd></dl></div>' % namespaces_string_html_default),
+            ('<page %s><list item-label-generate="unordered"><list-item><list-item-body>Item</list-item-body></list-item></list></page>' % ns_all,
+                '<div %s><ul><li>Item</li></ul></div>' % ns_html),
+            ('<page %s><list item-label-generate="ordered"><list-item><list-item-body>Item</list-item-body></list-item></list></page>' % ns_all,
+                '<div %s><ol><li>Item</li></ol></div>' % ns_html),
+            ('<page %s><list><list-item><list-item-label>Label</list-item-label><list-item-body>Item</list-item-body></list-item></list></page>' % ns_all,
+                '<div %s><dl><dt>Label</dt><dd>Item</dd></dl></div>' % ns_html),
         ]
         for i in pairs:
             yield (self._do, ) + i
 
     def test_object(self):
         pairs = [
-            ('<page %s><object xlink:href="href"/></page>' % ns_str_default,
-                '<div %s><object data="href" /></div>' % namespaces_string_html_default),
-            ('<page %s><object xlink:href="href.png"/></page>' % ns_str_default,
-                '<div %s><img src="href.png" /></div>' % namespaces_string_html_default),
+            ('<page %s><object xlink:href="href"/></page>' % ns_all,
+                '<div %s><object data="href" /></div>' % ns_html),
+            ('<page %s><object xlink:href="href.png"/></page>' % ns_all,
+                '<div %s><img src="href.png" /></div>' % ns_html),
         ]
         for i in pairs:
             yield (self._do, ) + i
 
     def test_style(self):
         pairs = [
-            ('<page %s><p font-size="1em">Text</p></page>' % namespaces_string_page_default,
-                '<div %s><p style="font-size: 1em">Text</p></div>' % namespaces_string_html_default),
-            ('<page %s %s><p html:style="color: black" font-size="1em">Text</p></page>' % (namespaces_string_page_default, namespaces_string_html),
-                '<div %s><p style="font-size: 1em; color: black">Text</p></div>' % namespaces_string_html_default),
+            ('<page %s><p font-size="1em">Text</p></page>' % ns_all,
+                '<div %s><p style="font-size: 1em">Text</p></div>' % ns_html),
+            ('<page %s><p html:style="color: black" font-size="1em">Text</p></page>' % ns_all,
+                '<div %s><p style="font-size: 1em; color: black">Text</p></div>' % ns_html),
         ]
         for i in pairs:
             yield (self._do, ) + i
 
     def test_table(self):
         pairs = [
-            ('<page %s><table><table-header><table-row><table-cell>Header</table-cell></table-row></table-header><table-footer><table-row><table-cell>Footer</table-cell></table-row></table-footer><table-body><table-row><table-cell>Cell</table-cell></table-row></table-body></table></page>' % namespaces_string_page_default,
-                '<div %s><table><thead><tr><td>Header</td></tr></thead><tfoot><tr><td>Footer</td></tr></tfoot><tbody><tr><td>Cell</td></tr></tbody></table></div>' % namespaces_string_html_default),
+            ('<page %s><table><table-header><table-row><table-cell>Header</table-cell></table-row></table-header><table-footer><table-row><table-cell>Footer</table-cell></table-row></table-footer><table-body><table-row><table-cell>Cell</table-cell></table-row></table-body></table></page>' % ns_all,
+                '<div %s><table><thead><tr><td>Header</td></tr></thead><tfoot><tr><td>Footer</td></tr></tfoot><tbody><tr><td>Cell</td></tr></tbody></table></div>' % ns_html),
         ]
         for i in pairs:
             yield (self._do, ) + i
@@ -123,8 +123,8 @@ class TestConverter(object):
 
     def test_macro(self):
         pairs = [
-            ('<page:page %s><page:macro><page:macro-body><page:p>Test</page:p></page:macro-body></page:macro></page:page>' % namespaces_string_page,
-                '<div %s %s><page:macro><page:macro-body><p>Test</p></page:macro-body></page:macro></div>' % (namespaces_string_html_default, namespaces_string_page)),
+            ('<page:page %s><page:macro><page:macro-body><page:p>Test</page:p></page:macro-body></page:macro></page:page>' % ns_all,
+                '<div %s %s><page:macro><page:macro-body><p>Test</p></page:macro-body></page:macro></div>' % (ns_html, ns_page)),
         ]
         for i in pairs:
             yield (self._do, ) + i
@@ -140,22 +140,22 @@ class TestConverterPage(object):
 
     def test_macro(self):
         pairs = [
-            ('<page:page %s><page:macro><page:macro-body><page:p>Test</page:p></page:macro-body></page:macro></page:page>' % namespaces_string_page,
-                '<div %s><p>Test</p></div>' % namespaces_string_html_default),
+            ('<page:page %s><page:macro><page:macro-body><page:p>Test</page:p></page:macro-body></page:macro></page:page>' % ns_all,
+                '<div %s><p>Test</p></div>' % ns_html),
         ]
         for i in pairs:
             yield (self._do, ) + i
 
     def test_note(self):
         pairs = [
-            ('<page %s><p>Text<note note-class="footnote"><note-body>Note</note-body></note></p></page>' % namespaces_string_page_default,
-                '<div %s><p>Text<sup id="note-1-ref"><a href="#note-1">1</a></sup></p><p id="note-1"><sup><a href="#note-1-ref">1</a></sup>Note</p></div>' % namespaces_string_html_default),
+            ('<page %s><p>Text<note note-class="footnote"><note-body>Note</note-body></note></p></page>' % ns_all,
+                '<div %s><p>Text<sup id="note-1-ref"><a href="#note-1">1</a></sup></p><p id="note-1"><sup><a href="#note-1-ref">1</a></sup>Note</p></div>' % ns_html),
         ]
         for i in pairs:
             yield (self._do, ) + i
 
     def test_unknown(self):
-        page = ET.XML("<page:unknown %s/>" % namespaces_string_page)
+        page = ET.XML("<page:unknown %s/>" % ns_all)
         py.test.raises(ElementException, self.conv.__call__, page)
 
     def _do(self, input, output):
