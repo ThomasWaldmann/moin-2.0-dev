c3185087fc1eee4d3c12864068d9849af4075a1d
Pawel Pacana <pawel.pacana@gmail.com>
2008-08-08 03:17:20 +0200

    (migration) Added 'source' option, wrong options handling. EDIT_LOG_MTIME to __timestamp.

--- MoinMoin/script/migration/backend.py
+++ MoinMoin/script/migration/backend.py
@@ -1,13 +1,20 @@
 # -*- coding: iso-8859-1 -*-
 """
     MoinMoin - backend migration script
-
+    
+    Recreate data from source backend either in data_backend or user_backend.    
+    Assumptions:
+    - defined user_backend/data_backend in wikiconfig
+    - defined migration source backend (default: migration_source in wikiconfig)
+    
+    TODO: tests
 
     @copyright: 2008 MoinMoin:PawelPacana
     @license: GNU GPL, see COPYING for details.
 """
 
-from MoinMoin.script import MoinScript
+from MoinMoin.script import MoinScript, fatal
+from MoinMoin.storage import EDIT_LOG_MTIME
 
 class PluginScript(MoinScript):
     """Backend migration class."""
@@ -17,26 +24,37 @@ class PluginScript(MoinScript):
             "-t", "--type", dest="backend_type",
             help="Migrate specified type of backend: user, data"
         )
+        self.parser.add_option(
+            "-s", "--source", dest="source_backend", default='migration_source',
+            help="Specify source of migration."
+        )
 
     def mainloop(self):
         self.init_request()
         request = self.request
         if self.options.backend_type == "user":
-            src_backend = request.cfg.user_backend
+            dst_backend = request.cfg.user_backend
         elif self.options.backend_type == "data":
-            src_backend = request.cfg.data_backend
-
-        # XXX: how to sanely define destination with all possible
-        # different config options?
-        # and what about migrating to/from MemoryBackend
-        dst_backend = request.cfg.migration_backend
-
+            dst_backend = request.cfg.data_backend
+        else:
+            fatal("Please, choose backend type [--type].")                
+        src = self.options.source_backend
+        try:
+            src_backend = getattr(request.cfg, src)
+        except AttributeError:
+            fatal("No such source backend: %s" % src)
+              
         def clone_item(backend, item):
             new_item = backend.create_item(item.name)
             for revno in item.list_revisions():  # revs
-                new_rev, rev = new_item.create_revision(revno), item.get_revision(revno)
-                rev.keys()  # loaded lazily
-                new_rev.update(rev)
+                rev, new_rev = item.get_revision(revno), new_item.create_revision(revno)                
+                rev.keys()  # metadata loaded lazily
+                # filter out deprecated meta
+                meta = dict(filter(lambda x: x[0] != EDIT_LOG_MTIME, rev.iteritems()))                                
+                # this didn't work: new_rev.update(rev)
+                new_rev._metadata.update(meta)
+                if not new_rev.has_key('__timestamp'):
+                    new_rev._metadata['__timestamp'] = rev[EDIT_LOG_MTIME]                   
                 new_rev.write(rev.read())
                 new_item.commit()
 
