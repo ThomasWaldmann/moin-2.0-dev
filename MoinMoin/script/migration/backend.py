564e1ff5f1eda6135f1ef4de681c12a54433f3ea
Pawel Pacana <pawel.pacana@gmail.com>
2008-08-08 18:34:22 +0200

    (migration) Code separation: now import 'clone' to use it elsewhere. Less parameters in CLI -> more in wikiconfig. Unused import purge.

--- MoinMoin/script/migration/backend.py
+++ MoinMoin/script/migration/backend.py
@@ -2,10 +2,10 @@
 """
     MoinMoin - backend migration script
 
-    Recreate data from source backend either in data_backend or user_backend.
+    Recreate data from source backend to destination.
     Assumptions:
     - defined user_backend/data_backend in wikiconfig
-    - defined migration source backend (default: migration_source in wikiconfig)
+    - defined user_backend_source/data_backend_source in wikiconfig
 
     TODO: tests, case for comparing already existing items (interrupted migration)
 
@@ -16,7 +16,6 @@
 import shutil
 
 from MoinMoin.script import MoinScript, fatal
-from MoinMoin.storage import EDIT_LOG_MTIME
 
 
 class PluginScript(MoinScript):
@@ -27,44 +26,45 @@ class PluginScript(MoinScript):
             "-t", "--type", dest="backend_type",
             help="Migrate specified type of backend: user, data"
         )
-        self.parser.add_option(
-            "-s", "--source", dest="source_backend", default='migration_source',
-            help="Specify source of migration."
-        )
 
     def mainloop(self):
         self.init_request()
         request = self.request
-        if self.options.backend_type == "user":
-            dst_backend = request.cfg.user_backend
-        elif self.options.backend_type == "data":
-            dst_backend = request.cfg.data_backend
-        else:
-            fatal("Please, choose backend type [--type].")
-        src = self.options.source_backend
         try:
-            src_backend = getattr(request.cfg, src)
+            if self.options.backend_type == "user":
+                dst_backend = request.cfg.user_backend
+                src_backend = request.cfg.user_backend_source
+            elif self.options.backend_type == "data":
+                dst_backend = request.cfg.data_backend
+                src_backend = request.cfg.data_backend_source
+            else:
+                fatal("Please, choose backend type [--type].")
         except AttributeError:
-            fatal("No such source backend: %s" % src)
+            fatal("Please, configure your %(user)s_backend and %(user)s_backend_source in wikiconfig.py." %
+                  {'user': self.options.backend_type})
+        clone(src_backend, dst_backend)
 
-        def clone_item(backend, item):
-            new_item = backend.create_item(item.name)
-            for revno in item.list_revisions():  # revs
-                rev, new_rev = item.get_revision(revno), new_item.create_revision(revno)
-                for k, v in rev.iteritems():
-                    try:
-                        new_rev[k] = v
-                    except TypeError:
-                        new_rev[k] = tuple(v)  # list to tuple
-                new_rev.timestamp = rev.timestamp
-                shutil.copyfileobj(rev, new_rev)
-                new_item.commit()
 
-            new_item.change_metadata()  # meta
-            for key in item.keys():
-                new_item[key] = item[key]
-            new_item.publish_metadata()
+def clone(source, destination):
+    """Clone items from source into destination backend."""
+    def clone_item(backend, item):
+        new_item = backend.create_item(item.name)
+        for revno in item.list_revisions():  # revs
+            rev, new_rev = item.get_revision(revno), new_item.create_revision(revno)
+            for k, v in rev.iteritems():
+                try:
+                    new_rev[k] = v
+                except TypeError:
+                    new_rev[k] = tuple(v)  # list to tuple
+            new_rev.timestamp = rev.timestamp
+            shutil.copyfileobj(rev, new_rev)
+            new_item.commit()
 
-        for item in src_backend.iteritems():
-            clone_item(dst_backend, item)
+        new_item.change_metadata()  # meta
+        for key in item.keys():
+            new_item[key] = item[key]
+        new_item.publish_metadata()
+
+    for item in source.iteritems():
+        clone_item(destination, item)
 
