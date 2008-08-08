0ba6e1ee01f962398730f511d4ea727962fa57a6
Christopher Denter <moin GUESSWHAT the DASH space DASH station ROUNDTHING com@localhost>
2008-08-09 00:32:41 +0200

    storage: FSBackend: Make Exception error more useful by providing further information

--- MoinMoin/storage/backends/fs.py
+++ MoinMoin/storage/backends/fs.py
@@ -216,8 +216,8 @@ class FSBackend(Backend):
         if revno in revs:
             raise RevisionAlreadyExistsError("A Revision with the number %d already exists on the item %r" % (revno, item.name))
         elif revno != last_rev + 1:
-            raise RevisionNumberMismatchError("The latest revision is %d, thus you cannot create revision number %d. \
-                                               The revision number must be latest_revision + 1." % (last_rev, revno))
+            raise RevisionNumberMismatchError("The latest revision of the item '%r' is %d, thus you cannot create revision number %d. \
+                                               The revision number must be latest_revision + 1." % (item.name, last_rev, revno))
 
         rev = NewRevision(item, revno)
         rev._revno = revno
