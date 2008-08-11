5d8f1b59387bbfd92d659195e81f0a2ed10fe8b0
Bastian Blank <bblank@thinkmo.de>
2008-08-11 17:31:41 +0200

    Macro base classes - Use tree element generator

--- MoinMoin/macro2/_base.py
+++ MoinMoin/macro2/_base.py
@@ -5,10 +5,8 @@ MoinMoin - Macro base class
 @license: GNU GPL, see COPYING for details.
 """
 
-from emeraldtree import ElementTree as ET
-
 from MoinMoin import wikiutil
-from MoinMoin.util import namespaces, uri
+from MoinMoin.util import tree, uri
 
 class MacroBase(object):
     """
@@ -56,7 +54,7 @@ class MacroInlineBase(MacroBase):
         ret = self.call_macro(content)
         if self.context == 'inline':
             return ret
-        return ET.Element(ET.QName('p', namespaces.moin_page), children=[ret])
+        return tree.moin_page.p(children=[ret])
 
 class MacroInlineOnlyBase(MacroBase):
     """
@@ -72,60 +70,40 @@ class MacroInlineOnlyBase(MacroBase):
 class MacroPageLinkListBase(MacroBlockBase):
     def create_pagelink_list(self, pagenames, ordered=False):
         """ creates an ET with a list of pagelinks from a list of pagenames """
-        tag_l = ET.QName('list', namespaces.moin_page)
-        attr_generate = ET.QName('item-label-generate', namespaces.moin_page)
-        tag_li = ET.QName('list-item', namespaces.moin_page)
-        tag_li_body = ET.QName('list-item-body', namespaces.moin_page)
-        tag_a = ET.QName('a', namespaces.moin_page)
-        attr_href_xlink = ET.QName('href', namespaces.xlink)
-
-        page_list = ET.Element(tag_l, attrib={attr_generate: ordered and 'ordered' or 'unordered'})
+        page_list = tree.moin_page.list(attrib={tree.moin_page.item_label_generate: ordered and 'ordered' or 'unordered'})
         for pagename in pagenames:
             # TODO: unicode URI
             # This link can never reach pagelinks
             url = str(uri.Uri(scheme='wiki', authority='', path='/' + pagename.encode('utf-8')))
-            pagelink = ET.Element(tag_a, attrib={attr_href_xlink: url}, children=[pagename])
-            item_body = ET.Element(tag_li_body, children=[pagelink])
-            item = ET.Element(tag_li, children=[item_body])
+            pagelink = tree.moin_page.a(attrib={tree.xlink.href: url}, children=[pagename])
+            item_body = tree.moin_page.list_item_body(children=[pagelink])
+            item = tree.moin_page.list_item(children=[item_body])
             page_list.append(item)
         return page_list
 
 class MacroNumberPageLinkListBase(MacroBlockBase):
     def create_number_pagelink_list(self, num_pagenames, ordered=False):
         """ creates an ET with a list of pagelinks from a list of pagenames """
-        tag_l = ET.QName('list', namespaces.moin_page)
-        attr_generate = ET.QName('item-label-generate', namespaces.moin_page)
-        tag_li = ET.QName('list-item', namespaces.moin_page)
-        tag_li_body = ET.QName('list-item-body', namespaces.moin_page)
-        tag_code = ET.QName('code', namespaces.moin_page)
-        tag_a = ET.QName('a', namespaces.moin_page)
-        attr_href_xlink = ET.QName('href', namespaces.xlink)
-
-        num_page_list = ET.Element(tag_l, attrib={attr_generate: ordered and 'ordered' or 'unordered'})
+        page_list = tree.moin_page.list(attrib={tree.moin_page.item_label_generate: ordered and 'ordered' or 'unordered'})
         for num, pagename in num_pagenames:
-            num_code = ET.Element(tag_code, children=["%6d " % num])
+            num_code = tree.moin_page.code(children=["%6d " % num])
             # TODO: unicode URI
             # This link can never reach pagelinks
             url = str(uri.Uri(scheme='wiki', authority='', path='/' + pagename.encode('utf-8')))
-            pagelink = ET.Element(tag_a, attrib={attr_href_xlink: url}, children=[pagename])
-            item_body = ET.Element(tag_li_body, children=[num_code, pagelink])
-            item = ET.Element(tag_li, children=[item_body])
+            pagelink = tree.moin_page.a(attrib={attr_href_xlink: url}, children=[pagename])
+            item_body = tree.moin_page.list_item_body(children=[num_code, pagelink])
+            item = tree.moin_page.list_item(children=[item_body])
             num_page_list.append(item)
         return num_page_list
 
 class MacroDefinitionListBase(MacroBlockBase):
     def create_definition_list(self, items):
         """ creates an ET with a definition list made from items """
-        tag_l = ET.QName('list', namespaces.moin_page)
-        tag_li = ET.QName('list-item', namespaces.moin_page)
-        tag_li_label = ET.QName('list-item-label', namespaces.moin_page)
-        tag_li_body = ET.QName('list-item-body', namespaces.moin_page)
-
-        def_list = ET.Element(tag_l)
+        def_list = tree.moin_page.list()
         for label, body in items:
-            item_label = ET.Element(tag_li_label, children=[label])
-            item_body = ET.Element(tag_li_body, children=[body])
-            item = ET.Element(tag_li, children=[item_label, item_body])
+            item_label = tree.moin_page.list_item_label(children=[label])
+            item_body = tree.moin_page.list_item_body(children=[body])
+            item = tree.moin_page.list_item(children=[item_label, item_body])
             def_list.append(item)
         return def_list
 
