442ba844635c4f9cf93f0adbce9645e4736f98f2
Bastian Blank <bblank@thinkmo.de>
2008-08-14 00:30:53 +0200

    Compatibility input converter - Cleanup

--- MoinMoin/converter2/compatibility_in.py
+++ MoinMoin/converter2/compatibility_in.py
@@ -8,17 +8,12 @@ compatibility formatter to create a converter.
 @license: GNU GPL, see COPYING for details.
 """
 
-import re
-from emeraldtree import ElementTree as ET
-
-from MoinMoin import config, wikiutil
+from MoinMoin import wikiutil
 from MoinMoin.Page import Page
-from MoinMoin.util import namespaces, uri
+from MoinMoin.util import uri
+from MoinMoin.util.tree import moin_page
 
 class Converter(object):
-    tag_page = ET.QName('page', namespaces.moin_page)
-    tag_page_href = ET.QName('page-href', namespaces.moin_page)
-
     def __init__(self, request, page_url, args, parser, formatter):
         self.request, self.page_url, self.args = request, page_url, args
         self.parser, self.formatter = parser, formatter
@@ -26,39 +21,42 @@ class Converter(object):
     def __call__(self, content):
         attrib = {}
         if self.page_url is not None:
-            attrib[self.tag_page_href] = self.page_url
+            attrib[moin_page.page_href] = self.page_url
 
-        self.root = ET.Element(self.tag_page, attrib=attrib)
+        root = moin_page.page(attrib=attrib)
 
         text = '\n'.join(content)
-        # TODO: Remove Page object
         # TODO: unicode URI
-        page = Page(self.request, uri.Uri(self.page_url).path.decode('utf-8')[1:])
+        url = uri.Uri(self.page_url)
+        # TODO: Remove Page object
+        page = Page(self.request, url.path.decode('utf-8')[1:])
 
         parser = self.parser(text, self.request, format_args=self.args)
         formatter = self.formatter(self.request, page)
 
         parser.format(formatter)
 
-        self.root.extend(formatter.root)
+        root.extend(formatter.root)
 
-        return self.root
+        return root
 
-def _factory(request, input, output):
+def _factory(request, _input, output):
     if output == 'application/x-moin-document':
         try:
-            Parser = wikiutil.searchAndImportPlugin(request.cfg, "parser", input)
-            Formatter = wikiutil.searchAndImportPlugin(request.cfg, "formatter", 'compatibility')
+            parser = wikiutil.searchAndImportPlugin(
+                    request.cfg, "parser", input)
+            formatter = wikiutil.searchAndImportPlugin(
+                    request.cfg, "formatter", 'compatibility')
         except wikiutil.PluginMissingError:
             return
 
         cls = type('Converter.%s' % str(input), (Converter, ), {})
         def init(self, request, page_url=None, args=None):
-            super(cls, self).__init__(request, page_url, args, Parser, Formatter)
+            super(cls, self).__init__(request, page_url, args, parser, formatter)
         cls.__init__ = init
 
         return cls
 
-from _registry import default_registry
+from MoinMoin.converter2._registry import default_registry
 # Need to register ourself after all normal parsers but before the wildcard
 default_registry.register(_factory, default_registry.PRIORITY_MIDDLE + 1)
