8c5b84155706f9f9d7185493d80dc8fcf00399ff
Bastian Blank <bblank@thinkmo.de>
2008-08-17 20:52:42 +0200

    Macro package - Add comment

--- MoinMoin/macro2/__init__.py
+++ MoinMoin/macro2/__init__.py
@@ -1,5 +1,12 @@
 """
-MoinMoin - New style macros, package
+MoinMoin - New style macros
+
+Macros are used to implement complex and/or dynamic page content.
+
+These new-style macros uses a class interface and always works on the internal
+tree representation of the document.
+
+TODO: Merge with converters
 
 @copyright: 2008 MoinMoin:BastianBlank
 @license: GNU GPL, see COPYING for details.
