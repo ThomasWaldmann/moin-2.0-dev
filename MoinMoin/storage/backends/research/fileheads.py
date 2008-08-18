ef87e7fc028bfd00fe4e7bb07f7d29c94ccb2a71
Pawel Pacana <pawel.pacana@gmail.com>
2008-08-18 18:16:36 +0200

    (storage) Removed obsoleted files.

--- MoinMoin/storage/backends/research/fileheads.py
+++
@@ -1,9 +0,0 @@
-from mercurial.node import short
-
-def show_file_heads(ui, repo, filename):
-    filelog = repo.file(filename)
-    for node in filelog.heads():
-        rev = filelog.linkrev(node)
-        print rev, ": ", short(repo[rev].node())
-
-cmdtable = {"fileheads|fh": (show_file_heads, [], "hg fileheads FILE")}
