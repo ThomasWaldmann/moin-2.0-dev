e64bde0c99f491fa9439753288f689dce2ded82c
TW <tw@waldmann-edv.de>
2009-05-25 20:43:49 +0200

    items: added some basic tests

---
+++ MoinMoin/items/_tests/test_Item.py
@@ -0,0 +1,72 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - MoinMoin.items Tests
+
+    @copyright: 2009 MoinMoin:ThomasWaldmann
+    @license: GNU GPL, see COPYING for details.
+"""
+
+import py
+
+from MoinMoin.items import Item, NonExistent, \
+                           DELETED, EDIT_LOG_ADDR, EDIT_LOG_EXTRA, EDIT_LOG_COMMENT, \
+                           EDIT_LOG_HOSTNAME, EDIT_LOG_USERID, EDIT_LOG_ACTION
+
+class TestItem:
+    def testNonExistent(self):
+        item = Item.create(self.request, 'DoesNotExist')
+        assert isinstance(item, NonExistent)
+        meta, data = item.meta, item.data
+        assert meta == {'mimetype': 'application/x-unknown'}
+        assert data == ''
+
+    def testCRUD(self):
+        name = u'NewItem'
+        mimetype = 'text/plain'
+        data = 'foobar'
+        meta = dict(foo='bar')
+        comment = u'saved it'
+        item = Item.create(self.request, name)
+        # save rev 0
+        item._save(meta, data, mimetype=mimetype, comment=comment)
+        # check save result
+        item = Item.create(self.request, name)
+        saved_meta, saved_data = dict(item.meta), item.data
+        assert saved_meta['mimetype'] == mimetype
+        assert saved_meta[EDIT_LOG_COMMENT] == comment
+        assert saved_data == data
+        assert item.rev.revno == 0
+
+        data = rev1_data = data * 10000
+        comment = comment + u' again'
+        # save rev 1
+        item._save(meta, data, mimetype=mimetype, comment=comment)
+        # check save result
+        item = Item.create(self.request, name)
+        saved_meta, saved_data = dict(item.meta), item.data
+        assert saved_meta['mimetype'] == mimetype
+        assert saved_meta[EDIT_LOG_COMMENT] == comment
+        assert saved_data == data
+        assert item.rev.revno == 1
+        assert not saved_meta.get(DELETED)
+
+        data = ''
+        comment = 'saved empty data'
+        # save rev 2 (auto delete)
+        item._save(meta, data, mimetype=mimetype, comment=comment)
+        # check save result
+        item = Item.create(self.request, name)
+        saved_meta, saved_data = dict(item.meta), item.data
+        assert saved_meta['mimetype'] == mimetype
+        assert saved_meta[EDIT_LOG_COMMENT] == comment
+        assert saved_data == data
+        assert item.rev.revno == 2
+        assert saved_meta[DELETED]
+
+        # access old revision
+        item = Item.create(self.request, name, rev_no=1)
+        assert item.data == rev1_data
+
+
+coverage_modules = ['MoinMoin.items']
+
