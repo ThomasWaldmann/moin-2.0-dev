a0a3586f100cff3ae5cf0a358f58a583402d01d7
Christopher Denter <moin GUESSWHAT the DASH space DASH station ROUNDTHING com@localhost>
2009-05-28 20:58:16 +0200

    storage: Add initial version if ACLMiddleWare testcases

---
+++ MoinMoin/storage/_tests/test_middleware_acl.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+    MoinMoin - Test - ACLMiddleWare
+
+    This defines tests for the ACLMiddleWare
+
+    @copyright: 2009 MoinMoin:ChristopherDenter,
+    @license: GNU GPL, see COPYING for details.
+"""
+from MoinMoin.Page import ACL
+from MoinMoin.storage.backends.acl import AccessDeniedError
+
+import py
+
+
+class TestACLMiddleware(object):
+    """
+    Test the AMW
+    """
+
+    def get_item(self, name):
+        # Just as a shortcut
+        return self.request.data_backend.get_item(name)
+
+    def create_item_acl(self, name, acl):
+        item = self.request.data_backend.create_item(name)
+        rev = item.create_revision(0)
+        rev[ACL] = acl
+        item.commit()
+
+    def test_noaccess(self):
+        access = "noaccess"
+        self.create_item_acl(access, "All:")
+        assert py.test.raises(AccessDeniedError, self.get_item, access)
+
+    def test_read_access_allowed(self):
+        access = "readaccessallowed"
+        self.create_item_acl(access, "All:read")
+        # Should simply pass...
+        item = self.get_item(access)
+
+        # Should not...
+        assert py.test.raises(AccessDeniedError, item.create_revision, 1)
