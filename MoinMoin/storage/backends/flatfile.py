944beafa5efc0610990a441d4c7e461b5fa4d6ce
TW <tw@waldmann-edv.de>
2009-06-06 00:03:48 +0200

    storage: flatfile backend: more cleanup, small fixes

--- MoinMoin/storage/backends/flatfile.py
+++ MoinMoin/storage/backends/flatfile.py
@@ -23,12 +23,15 @@
 import os, re
 from cStringIO import StringIO
 
+from MoinMoin import log
+logging = log.getLogger(__name__)
+
 from MoinMoin.storage import Backend, Item, StoredRevision, NewRevision
 from MoinMoin.storage.error import NoSuchItemError, NoSuchRevisionError, \
                                    ItemAlreadyExistsError, \
                                    RevisionAlreadyExistsError
 from MoinMoin.wikiutil import add_metadata_to_body, split_body, quoteWikinameFS, unquoteWikiname
-from MoinMoin.items import EDIT_LOG, EDIT_LOG_ACTION
+from MoinMoin.items import MIMETYPE, EDIT_LOG, EDIT_LOG_ACTION
 
 class FlatFileBackend(Backend):
     def __init__(self, path):
@@ -39,9 +42,6 @@ class FlatFileBackend(Backend):
         """
         self._path = path
 
-    _noquote_re = re.compile('[A-Za-z0-9 ()-]')
-    _unquote_re = re.compile('_[a-fA-F0-9]{2}|.')
-
     def _quote(self, name):
         return quoteWikinameFS(name)
 
@@ -51,6 +51,10 @@ class FlatFileBackend(Backend):
     def _rev_path(self, name):
         return os.path.join(self._path, self._quote(name))
 
+    def _exists(self, name):
+        revpath = self._rev_path(name)
+        return os.path.exists(revpath)
+
     def history(self, reverse=True):
         rev_list = [i.get_revision(-1) for i in self.iteritems()]
         rev_list.sort(lambda x, y: cmp(x.timestamp, y.timestamp))
@@ -59,10 +63,12 @@ class FlatFileBackend(Backend):
         return iter(rev_list)
 
     def get_item(self, itemname):
+        if not self._exists(itemname):
+            raise NoSuchItemError("No such item, %r" % (itemname))
         return Item(self, itemname)
 
     def has_item(self, itemname):
-        return self.get_item(itemname) is not None
+        return self._exists(itemname)
 
     def create_item(self, itemname):
         if not isinstance(itemname, (str, unicode)):
@@ -91,15 +97,16 @@ class FlatFileBackend(Backend):
         for e in EDIT_LOG:
             rev._metadata[e] = ''
         rev._metadata[EDIT_LOG_ACTION] = 'SAVE'
+        rev._metadata[MIMETYPE] = 'text/moin-wiki' # XXX get this from PI
         rev._data = StringIO(data)
         rev._data_size = len(data)
         return rev
 
     def _list_revisions(self, item):
-        revpath = self._rev_path(item.name)
-        if os.path.exists(revpath):
+        if self._exists(item.name):
             return [0]
-        return []
+        else:
+            return []
 
     def _create_revision(self, item, revno):
         assert revno <= 1
@@ -109,8 +116,7 @@ class FlatFileBackend(Backend):
 
     def _rename_item(self, item, newname):
         try:
-            os.rename(self._rev_path(item.name),
-                      self._rev_path(newname))
+            os.rename(self._rev_path(item.name), self._rev_path(newname))
         except OSError:
             raise ItemAlreadyExistsError('')
 
