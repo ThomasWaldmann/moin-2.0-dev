dc3b240d58cafec7e59dbbff1752ba6273e6adbc
Reimar Bauer <rb.proj AT googlemail DOT com@localhost>
2009-06-06 23:46:32 +0200

    PackageItems: refactored to store items instead of pages or attachments

--- MoinMoin/action/PackageItems.py
+++ MoinMoin/action/PackageItems.py
@@ -16,133 +16,54 @@ import zipfile
 from datetime import datetime
 
 from MoinMoin import wikiutil, config, user
+from MoinMoin.items import Item
 from MoinMoin.Page import Page
-from MoinMoin.action.AttachFile import _addLogEntry
 from MoinMoin.packages import MOIN_PACKAGE_FILE, packLine, unpackLine
-from MoinMoin.action import AttachFile
-from MoinMoin.action.AttachFile import _get_files
 from MoinMoin.search import searchPages
+
+ACTION_NAME = __name__.split('.')[-1]
 
 class ActionError(Exception):
     pass
 
-class PackagePages:
-    def __init__(self, pagename, request):
+class PackageItems:
+    def __init__(self, item_name, request):
         self.request = request
-        self.pagename = pagename
-        self.page = Page(request, pagename)
-
-    def allowed(self):
-        """ Check if user is allowed to do this. """
-        return not self.__class__.__name__ in self.request.cfg.actions_excluded
+        self.item_name = item_name
 
     def render(self):
-        """ Render action
-
-        This action returns a wiki page with optional message, or
-        redirects to new page.
-        """
-        _ = self.request.getText
-
-        if 'cancel' in self.request.values:
-            # User canceled
-            return self.page.send_page()
-
-        try:
-            if not self.allowed():
-                self.request.theme.add_msg(_('You are not allowed to edit this page.'), "error")
-                raise ActionError
-            elif not self.page.exists():
-                self.request.theme.add_msg(_('This page is already deleted or was never created!'))
-                raise ActionError
-
-            self.package()
-        except ActionError, e:
-            return self.page.send_page()
-
-    def package(self):
         """ Calls collectpackage() with the arguments specified. """
         _ = self.request.getText
+        request = self.request
 
-        # Get new name from form and normalize.
-        pagelist = self.request.values.get('pagelist', u'')
-        packagename = self.request.values.get('packagename', u'')
-        include_attachments = self.request.values.get('include_attachments', False)
-
-        if not self.request.values.get('submit'):
-            self.request.theme.add_msg(self.makeform(), "dialog")
+        if self.__class__.__name__ in request.cfg.actions_excluded:
             raise ActionError
-
-        target = wikiutil.taintfilename(packagename)
+        if not request.values.get('button_ok'):
+            template = request.theme.env.get_template('action_packageitems.html')
+            content = template.render(gettext=request.getText,
+                          action=ACTION_NAME,
+                          label='package items',
+                          item_list=self.item_name,
+                          )
+            request.theme.render_content(self.item_name, content)
+            return
+        # Get new name from form and normalize.
+        target = request.form.get('target')
+        item_list = request.form.get('item_list')
+        include_subitems = request.values.get('sub_items', False)
+        
+        target = wikiutil.taintfilename(target)
 
         if not target:
-            self.request.theme.add_msg(self.makeform(_('Invalid filename "%s"!') % wikiutil.escape(packagename)), "error")
             raise ActionError
 
-        request = self.request
         filelike = cStringIO.StringIO()
-        package = self.collectpackage(unpackLine(pagelist, ","), filelike, target, include_attachments)
+        package = self.collectpackage(unpackLine(item_list, ","), filelike, target, include_subitems)
         request.content_type = 'application/zip'
         request.content_length = filelike.tell()
         request.headers.add('Content-Disposition', 'inline; filename="%s"' % target)
         request.write(filelike.getvalue())
         filelike.close()
-
-    def makeform(self, error=""):
-        """ Display a package page form
-
-        The form might contain an error that happened when package file was not given.
-        """
-        from MoinMoin.widget.dialog import Dialog
-        _ = self.request.getText
-
-        if error:
-            error = u'<p class="error">%s</p>\n' % error
-
-        d = {
-            'url': self.request.href(self.pagename),
-            'error': error,
-            'action': self.__class__.__name__,
-            'pagename': wikiutil.escape(self.pagename, True),
-            'include_attachments_label': _('Include all attachments?'),
-            'package': _('Package pages'),
-            'cancel': _('Cancel'),
-            'newname_label': _("Package name"),
-            'list_label': _("List of page names - separated by a comma"),
-        }
-        form = '''
-%(error)s
-<form method="post" action="%(url)s">
-<input type="hidden" name="action" value="%(action)s">
-<table>
-    <tr>
-        <td class="label"><label>%(newname_label)s</label></td>
-        <td class="content">
-            <input type="text" name="packagename" value="package.zip" size="80">
-        </td>
-    </tr>
-    <tr>
-        <td class="label"><label>%(list_label)s</label></td>
-        <td class="content">
-            <input type="text" name="pagelist" size="80" maxlength="200" value="%(pagename)s">
-        </td>
-    </tr>
-    <tr>
-        <td class="label">
-        %(include_attachments_label)s<input type="checkbox" name="include_attachments" value="0" %(include_attachments_label)s>
-    </td>
-    </tr>
-    <tr>
-        <td></td>
-        <td class="buttons">
-            <input type="submit" name="submit" value="%(package)s">
-            <input type="submit" name="cancel" value="%(cancel)s">
-        </td>
-    </tr>
-</table>
-</form>''' % d
-
-        return Dialog(self.request, content=form)
 
     def searchpackage(self, request, searchkey):
         """ Search MoinMoin for the string specified and return a list of
@@ -163,61 +84,49 @@ class PackagePages:
                 titles.append(title.page_name)
         return titles
 
-    def collectpackage(self, pagelist, fileobject, pkgname="", include_attachments=False):
-        """ Expects a list of pages as an argument, and fileobject to be an open
+    def collectpackage(self, item_list, fileobject, pkgname="", include_subitems=False):
+        """ Expects a list of items as an argument, and fileobject to be an open
         file object, which a zipfile will get written to.
 
-        @param pagelist: pages to package
+        @param item_list: items to package
         @param fileobject: open file object to write to
         @param pkgname: optional file name, to prevent self packaging
         @rtype: string or None
-        @return: error message, if one happened
-        @rtype: boolean
-        @param include_attachments: True if you want attachments collected
+        @param include_subitems: True if you want subitems collected
         """
         _ = self.request.getText
+        comment = "Created by the PackageItems action."
         COMPRESSION_LEVEL = zipfile.ZIP_DEFLATED
-
-        pages = []
-        for pagename in pagelist:
-            pagename = wikiutil.normalize_pagename(pagename, self.request.cfg)
-            if pagename:
-                page = Page(self.request, pagename)
-                if page.exists() and self.request.user.may.read(pagename):
-                    pages.append(page)
-        if not pages:
-            return (_('No pages like "%s"!') % wikiutil.escape(pagelist))
-
-        # Set zipfile output
         zf = zipfile.ZipFile(fileobject, "w", COMPRESSION_LEVEL)
-
         cnt = 0
         userid = user.getUserIdentification(self.request)
         script = [packLine(['MoinMoinPackage', '1']), ]
-
-        for page in pages:
+        rev_no = -1
+        for item_name in item_list:
+            item_name = item_name.strip()
             cnt += 1
-            files = _get_files(self.request, page.page_name)
-            script.append(packLine(["AddRevision", str(cnt), page.page_name, userid, "Created by the PackagePages action."]))
-
-            timestamp = page.mtime()
+            item = self.request.cfg.data_backend.get_item(item_name)
+            r = item.get_revision(rev_no)
+            mimetype = r.get('mimetype') or 'application/x-unknown' # XXX why do we need ... or ..?
+            contenttype = 'utf-8'
+            script.append(packLine(["AddItem", str(cnt), item_name, mimetype, contenttype, userid, comment]))
+            timestamp = float(r.timestamp)
             zi = zipfile.ZipInfo(filename=str(cnt), date_time=datetime.fromtimestamp(timestamp).timetuple()[:6])
             zi.compress_type = COMPRESSION_LEVEL
-            zf.writestr(zi, page.get_raw_body().encode("utf-8"))
-            if include_attachments:
-                for attname in files:
-                    if attname != pkgname:
-                        cnt += 1
-                        zipname = "%d_attachment" % cnt
-                        script.append(packLine(["AddAttachment", zipname, attname, page.page_name, userid, "Created by the PackagePages action."]))
-                        filename = AttachFile.getFilename(self.request, page.page_name, attname)
-                        zf.write(filename, zipname)
-        script += [packLine(['Print', 'Thank you for using PackagePages!'])]
-
+            zf.writestr(zi, r.read_data().encode("utf-8"))
+            if include_subitems:
+                for fullname, relname, mimetype in r.flat_index:
+                    contenttype = 'utf-8'
+                    sub_item = request.cfg.data_backend.get_item(fullname)
+                    rs = sub_item.get_revision(rev_no)
+                    cnt += 1
+                    script.append(packLine(["AddItem", str(cnt), fullname, mimetype, contenttype, userid, comment]))
+                    zf.write(rs.read_data(), str(cnt))
+        script += [packLine(['Print', 'Thank you for using PackageItems!'])]
         zf.writestr(MOIN_PACKAGE_FILE, u"\n".join(script).encode("utf-8"))
         zf.close()
+        
 
-def execute(pagename, request):
+def execute(item_name, request):
     """ Glue code for actions """
-    PackagePages(pagename, request).render()
-
+    PackageItems(item_name, request).render()
