5f686b28c9b255ac9193c511a01053880e0a315e
Bastian Blank <bblank@thinkmo.de>
2009-06-21 17:01:39 +0200

    Macro base classes - Use new macro interface

--- MoinMoin/macro2/_base.py
+++ MoinMoin/macro2/_base.py
@@ -12,25 +12,15 @@ from MoinMoin.util.tree import moin_page, xlink
 class MacroBase(object):
     """
     Macro base class.
-
-    Supports argument parsing with wikiutil.invoke_extension_function.
     """
 
     # The output of a immutable macro only depends on the arguments and the content
     immutable = False
 
-    def __init__(self, request, page_name, alt, context, args=None):
-        self.request, self.page_name = request, page_name
-        self.alt, self.context, self._args = alt, context, args
+    def __init__(self, request):
+        self.request = request
 
-    def call_macro(self, content):
-        try:
-            return wikiutil.invoke_extension_function(self.request, self.macro, self._args)
-        except ValueError:
-            # TODO: Real error
-            return 'Error'
-
-    def macro(self):
+    def __call__(self, content, page_url, args, alt, context_block):
         raise NotImplementedError
 
 class MacroBlockBase(MacroBase):
@@ -40,10 +30,13 @@ class MacroBlockBase(MacroBase):
     The macro gets only expanded in block context. In inline context the
     alternative text is used instead.
     """
-    def __call__(self, content=()):
-        if self.context == 'block':
-            return self.call_macro(content)
+    def __call__(self, content, page_url, args, alt, context_block):
+        if context_block:
+            return self.macro(content, page_url, args, alt)
         return self.alt
+
+    def macro(self, content, page_url, args, alt):
+        raise NotImplementedError
 
 class MacroInlineBase(MacroBase):
     """
@@ -51,22 +44,22 @@ class MacroInlineBase(MacroBase):
 
     The macro is wrapped into a paragraph in block context.
     """
-    def __call__(self, content=()):
-        ret = self.call_macro(content)
-        if self.context == 'inline':
-            return ret
-        return moin_page.p(children=[ret])
+    def __call__(self, content, page_url, args, alt, context_block):
+        ret = self.macro(content, page_url, args, alt)
+        if context_block:
+            return moin_page.p(children=(ret, ))
+        return ret
 
 class MacroInlineOnlyBase(MacroBase):
     """
     Macro base class for strict inline element macros.
 
-    The macro is onl< expanded in inline context. In block context it expands
+    The macro is only expanded in inline context. In block context it expands
     to nothing.
     """
-    def __call__(self, content=()):
-        if self.context == 'inline':
-            return self.call_macro(content)
+    def __call__(self, content, page_url, args, alt, context_block):
+        if not content_block:
+            return self.macro(content, page_url, args, alt)
 
 class MacroPageLinkListBase(MacroBlockBase):
     def create_pagelink_list(self, pagenames, ordered=False):
