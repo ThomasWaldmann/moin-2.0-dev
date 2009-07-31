3ec6b6cef26f2c0dd90104d8c3e3751d6415d999
Christopher Denter <moin GUESSWHAT the DASH space DASH station ROUNDTHING com@localhost>
2009-07-31 17:02:41 +0200

    storage: Redo the syspage_upgrade action. Use proper jinja template.

--- MoinMoin/action/syspages_upgrade.py
+++ MoinMoin/action/syspages_upgrade.py
@@ -5,63 +5,40 @@
     The superuser can select an item containing system pages that should be installed.
     It is also possible to upgrade already existing system pages this way.
 
-    @copyright: 2009 MoinMoin:ReimarBauer,
-                     MoinMoin:ThomasWaldmann,
-                     MoinMoin:ChristopherDenter
+    @copyright: 2009 MoinMoin:ChristopherDenter
 
     @license: GNU GPL, see COPYING for details.
 """
 
-from MoinMoin import i18n, packages
-from MoinMoin.i18n import strings
-i18n.strings = strings
-
-from MoinMoin.util.dataset import TupleDataset, Column
-from MoinMoin.widget.browser import DataBrowserWidget
-
 from MoinMoin.storage import upgrade_syspages
 from MoinMoin.storage.error import BackendError
 
-def execute(pagename, request):
-    if not request.user or not request.user.isSuperUser():
-        return ''
+
+def execute(item_name, request):
     _ = request.getText
-    fmt = request.html_formatter
+    if request.method == 'GET':
+        env = request.theme.env
+        template = env.get_template('action_query.html')
+        action = 'syspages_upgrade'
+        label = 'Upgrade System Pages'
+        content = template.render(gettext=request.getText,
+                                  action=action,
+                                  label=label,
+                                  no_comment=True,
+                                  target=' '  # stupid template...
+                                  )
+    elif request.method == 'POST':
+        cancelled = 'button_cancel' in request.form
+        if not cancelled:
+            syspages = request.form.get('target')
+            try:
+                # XXX in order to avoid a cyclic import... improve!
+                from MoinMoin.storage.backends import memory
+                upgrade_syspages(request, syspages, memory.MemoryBackend())
+            except BackendError, e:
+                content = _('<br> System pages upgrade failed due to the following error: %s.' % e)
+            else:
+                content = _('<br> System pages have been upgraded successfully!')
 
-    lang = request.values.get('language') or 'English'
-    syspages = request.values.get('syspages_package') or ''
-    msg = (_('Please specify a the system pages that you would like to install.'), 'info')
-
-    if syspages:
-        try:
-            # XXX in order to avoid a cyclic import... improve!
-            from MoinMoin.storage.backends import memory
-            upgrade_syspages(request, syspages, memory.MemoryBackend())
-        except BackendError, e:
-            msg = (_('System pages upgrade failed due to the following error: %s.' % e), 'error')
-        else:
-            msg = (_('System pages have been upgraded successfully!'), 'info')
-
-    data = TupleDataset()
-    data.columns = [
-           Column('syspage', label=_('System Pages')),
-           Column('action', label=_('Install')),
-        ]
-
-    label_install = _("install")
-
-    table = DataBrowserWidget(request)
-    table.setData(data)
-    page_table = ''.join(table.format(method='GET'))
-
-    fmt = request.formatter
-
-    title = _("Install or upgrade system pages")
-    request.theme.add_msg(*msg)
-    request.theme.send_title(title, page=request.page, pagename=pagename)
-    request.write(request.formatter.startContent("content"))
-    request.write(page_table)
-    request.write(request.formatter.endContent())
-    request.theme.send_footer(pagename)
-    request.theme.send_closing_html()
+    request.theme.render_content(item_name, content)
 
