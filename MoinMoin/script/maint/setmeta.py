549f480c7e73aa9982a66d19c6d94ee0e15efee8
Christopher Denter <moin GUESSWHAT the DASH space DASH station ROUNDTHING com@localhost>
2009-08-07 13:38:20 +0200

    storage: Introduce maintenance setmeta script

---
+++ MoinMoin/script/maint/setmeta.py
@@ -0,0 +1,69 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - Set Revision Metadata
+
+    This script can be used to duplicate the last revision of
+    every item in the backend and set or change (additional)
+    metadata of the new revision.
+
+    @copyright: 2009 MoinMoin:ChristopherDenter
+    @license: GNU GPL, see COPYING for details.
+"""
+
+from shutil import copyfileobj
+from os.path import splitext
+
+from MoinMoin.wsgiapp import init_unprotected_backends
+from MoinMoin.script import MoinScript, fatal
+from MoinMoin.items import IS_SYSPAGE, SYSPAGE_VERSION
+from MoinMoin.storage.error import NoSuchRevisionError
+
+class PluginScript(MoinScript):
+    """System Page Tagging Script"""
+    def __init__(self, argv, def_values):
+        MoinScript.__init__(self, argv, def_values)
+        self.parser.add_option(
+            "-k", "--key", dest="key", action="store", type='string',
+            help="The key you want to set/change in the new revision"
+        )
+        self.parser.add_option(
+            "-v", "--value", dest="value", action="store", type='string',
+            help="The system page version you would like to create."
+        )
+        # TODO: Support
+#        self.parser.add_option(
+#            "-p", "--pattern", dest="pattern", action="store", type='string',
+#            help="Only set the metadata on items whose names match the pattern."
+#        )
+
+    def mainloop(self):
+        self.init_request()
+        request = self.request
+        init_unprotected_backends(request)
+        storage = request.unprotected_storage
+
+        key = self.options.key
+        val = self.options.value
+        if not (key and val):
+            fatal("You need to specify a proper key/value pair! Given: key:%r, value:%r" % (key, val))
+
+        # TODO only iterate over matching items via backend.search_item
+        for item in storage.iteritems():
+            try:
+                last_rev = item.get_revision(-1)
+            except NoSuchRevisionError:
+                last_rev = None
+
+            next_rev = item.create_revision(item.next_revno)
+
+            if last_rev:
+                # Copy data.
+                copyfileobj(last_rev, next_rev)
+                # Copy metadata:
+                for k, v in last_rev.iteritems():
+                    next_rev[k] = v
+
+            # Set or overwrite given metadata key with value
+            next_rev[key] = eval(val)
+            item.commit()
+
