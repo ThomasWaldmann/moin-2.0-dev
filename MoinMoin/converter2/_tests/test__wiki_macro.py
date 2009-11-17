4de7efc0e04179d01642be977d983f0802a4a25b
Bastian Blank <bblank@thinkmo.de>
2009-11-17 13:43:00 +0100

    Tests for macro handling - Remove namespaces before comparing the output with the expected result

--- MoinMoin/converter2/_tests/test__wiki_macro.py
+++ MoinMoin/converter2/_tests/test__wiki_macro.py
@@ -5,41 +5,35 @@ MoinMoin - Tests for MoinMoin.converter2._wiki_macro
 @license: GNU GPL, see COPYING for details.
 """
 
-from emeraldtree.ElementTree import ElementTree
 import py.test
+import re
 
 from MoinMoin.converter2._args import Arguments
+from MoinMoin.util.tree import xlink
 
 from MoinMoin.converter2._wiki_macro import *
 
-namespaces_string = 'xmlns="%s"' % moin_page.namespace
-namespaces_string_xinclude = 'xmlns:xi="%s"' % xinclude.namespace
-namespaces_xpstring = 'xmlns(page=%s)' % moin_page.namespace
-
-namespaces_list = {
-    moin_page.namespace: '',
-    xinclude.namespace: 'xi',
-}
-
-def serialize(elem, **options):
-    from cStringIO import StringIO
-    file = StringIO()
-    tree = ElementTree(elem)
-    tree.write(file, namespaces=namespaces_list, **options)
-    return file.getvalue()
 
 class TestConverter(object):
+    namespaces = {
+        moin_page.namespace: '',
+        xinclude.namespace: 'xi',
+        xlink.namespace: 'xlink',
+    }
+
+    output_re = re.compile(r'(\s+xmlns(:\w+)?="[^"]+"|xmlns\(\w+=[^)]+\)\s+)')
+
     def setup_class(self):
         self.conv = ConverterMacro(self.request)
 
     def test_macro(self):
         data = [
             ('Macro', None, 'text',
-                '<page alt="text" content-type="x-moin/macro;name=Macro" %s />' % namespaces_string,
-                '<inline-part alt="text" content-type="x-moin/macro;name=Macro" %s />' % namespaces_string),
+                '<page alt="text" content-type="x-moin/macro;name=Macro" />',
+                '<inline-part alt="text" content-type="x-moin/macro;name=Macro" />'),
             ('Macro', Arguments([u'arg1']), 'text',
-                '<page alt="text" content-type="x-moin/macro;name=Macro" %s><arguments><argument>arg1</argument></arguments></page>' % namespaces_string,
-                '<inline-part alt="text" content-type="x-moin/macro;name=Macro" %s><arguments><argument>arg1</argument></arguments></inline-part>' % namespaces_string),
+                '<page alt="text" content-type="x-moin/macro;name=Macro"><arguments><argument>arg1</argument></arguments></page>',
+                '<inline-part alt="text" content-type="x-moin/macro;name=Macro"><arguments><argument>arg1</argument></arguments></inline-part>'),
         ]
         for name, args, text, output_block, output_inline in data:
             yield (self._do, name, args, text, True, output_block)
@@ -48,13 +42,13 @@ class TestConverter(object):
     def test_macro_arguments(self):
         data = [
             ('Macro', None, 'text',
-                '<inline-part alt="text" content-type="x-moin/macro;name=Macro" %s />' % namespaces_string),
+                '<inline-part alt="text" content-type="x-moin/macro;name=Macro" />'),
             ('Macro', Arguments([u'arg1', u'arg2']), 'text',
-                '<inline-part alt="text" content-type="x-moin/macro;name=Macro" %s><arguments><argument>arg1</argument><argument>arg2</argument></arguments></inline-part>' % namespaces_string),
+                '<inline-part alt="text" content-type="x-moin/macro;name=Macro"><arguments><argument>arg1</argument><argument>arg2</argument></arguments></inline-part>'),
             ('Macro', Arguments([], {'key': 'value'}), 'text',
-                '<inline-part alt="text" content-type="x-moin/macro;name=Macro" %s><arguments><argument name="key">value</argument></arguments></inline-part>' % namespaces_string),
+                '<inline-part alt="text" content-type="x-moin/macro;name=Macro"><arguments><argument name="key">value</argument></arguments></inline-part>'),
             ('Macro', Arguments([u'arg1', u'arg2'], {'key': 'value'}), 'text',
-                '<inline-part alt="text" content-type="x-moin/macro;name=Macro" %s><arguments><argument>arg1</argument><argument>arg2</argument><argument name="key">value</argument></arguments></inline-part>' % namespaces_string),
+                '<inline-part alt="text" content-type="x-moin/macro;name=Macro"><arguments><argument>arg1</argument><argument>arg2</argument><argument name="key">value</argument></arguments></inline-part>'),
         ]
         for name, args, text, output in data:
             yield (self._do, name, args, text, False, output)
@@ -63,35 +57,41 @@ class TestConverter(object):
         data = [
             ('BR', None, 'text',
                 None,
-                '<line-break %s />' % namespaces_string),
+                '<line-break />'),
             ('FootNote', Arguments([u'note']), 'text',
-                '<p %s><note note-class="footnote"><note-body>note</note-body></note></p>' % namespaces_string,
-                '<note note-class="footnote" %s><note-body>note</note-body></note>' % namespaces_string),
+                '<p><note note-class="footnote"><note-body>note</note-body></note></p>',
+                '<note note-class="footnote"><note-body>note</note-body></note>'),
             ('TableOfContents', None, 'text',
-                '<table-of-content %s />' % namespaces_string,
+                '<table-of-content />',
                 'text'),
             ('Include', Arguments([u'page']), 'text',
-                '<xi:include xi:href="wiki.local:page" %s />' % namespaces_string_xinclude,
+                '<xi:include xi:href="wiki.local:page" />',
                 'text'),
             ('Include', Arguments([u'^page']), 'text',
-                '<xi:include xi:xpointer="%s page:include(pages(^^page))" %s />' % (namespaces_xpstring, namespaces_string_xinclude),
+                '<xi:include xi:xpointer="page:include(pages(^^page))" />',
                 'text'),
             ('Include', Arguments([u'^page'], {u'sort': u'ascending'}), 'text',
-                '<xi:include xi:xpointer="%s page:include(pages(^^page) sort(ascending))" %s />' % (namespaces_xpstring, namespaces_string_xinclude),
+                '<xi:include xi:xpointer="page:include(pages(^^page) sort(ascending))" />',
                 'text'),
             ('Include', Arguments([u'^page'], {u'sort': u'descending'}), 'text',
-                '<xi:include xi:xpointer="%s page:include(pages(^^page) sort(descending))" %s />' % (namespaces_xpstring, namespaces_string_xinclude),
+                '<xi:include xi:xpointer="page:include(pages(^^page) sort(descending))" />',
                 'text'),
             ('Include', Arguments([u'^page'], {u'items': u'5'}), 'text',
-                '<xi:include xi:xpointer="%s page:include(pages(^^page) items(5))" %s />' % (namespaces_xpstring, namespaces_string_xinclude),
+                '<xi:include xi:xpointer="page:include(pages(^^page) items(5))" />',
                 'text'),
             ('Include', Arguments([u'^page'], {u'skipitems': u'5'}), 'text',
-                '<xi:include xi:xpointer="%s page:include(pages(^^page) skipitems(5))" %s />' % (namespaces_xpstring, namespaces_string_xinclude),
+                '<xi:include xi:xpointer="page:include(pages(^^page) skipitems(5))" />',
                 'text'),
         ]
         for name, args, text, output_block, output_inline in data:
             yield (self._do, name, args, text, True, output_block)
             yield (self._do, name, args, text, False, output_inline)
+
+    def serialize(self, elem, **options):
+        from StringIO import StringIO
+        file = StringIO()
+        elem.write(file.write, namespaces=self.namespaces, **options)
+        return self.output_re.sub(u'', file.getvalue())
 
     def _do(self, name, args, text, context_block, output):
         result = self.conv.macro(name, args, text, context_block)
@@ -99,5 +99,5 @@ class TestConverter(object):
             if isinstance(result, basestring):
                 assert result == output
             else:
-                assert serialize(result) == output
+                assert self.serialize(result) == output
 
