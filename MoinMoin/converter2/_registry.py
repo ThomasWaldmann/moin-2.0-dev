fd37cf5359cb1e5a2fd59d8b6628d811f6aef970
Bastian Blank <bblank@thinkmo.de>
2009-12-02 21:05:37 +0100

    Factor out base registry

--- MoinMoin/converter2/_registry.py
+++ MoinMoin/converter2/_registry.py
@@ -5,47 +5,7 @@ MoinMoin - Converter registry
 @license: GNU GPL, see COPYING for details.
 """
 
-_marker = object()
-
-class RegistryBase(object):
-    PRIORITY_REALLY_FIRST = -20
-    PRIORITY_FIRST = -10
-    PRIORITY_MIDDLE = 0
-    PRIORITY_LAST = 10
-    PRIORITY_REALLY_LAST = 20
-
-    class _Entry(object):
-        def __init__(self, factory, priority):
-            self.factory, self.priority = factory, priority
-
-        def __cmp__(self, other):
-            if isinstance(other, self.__class__):
-                return cmp(self.factory, other.factory)
-            return cmp(self.factory, other)
-
-    def __init__(self):
-        self._converters = []
-
-    def _get(self, *args, **kw):
-        for entry in self._converters:
-            conv = entry.factory(*args, **kw)
-            if conv is not None:
-                return conv
-
-    def _sort(self):
-        self._converters.sort(key=lambda a: a.priority)
-
-    def get(self, *args, **kw):
-        raise NotImplementedError
-
-    def register(self, factory, priority=PRIORITY_MIDDLE):
-        if factory not in self._converters:
-            self._converters.append(self._Entry(factory, priority))
-            self._sort()
-
-    def unregister(self, factory):
-        self._converters.remove(factory)
-        self._sort()
+from MoinMoin.util.registry import RegistryBase
 
 class Registry(RegistryBase):
     def get(self, request, input, output):
---
+++ MoinMoin/util/registry.py
@@ -0,0 +1,47 @@
+"""
+MoinMoin - Module registry
+
+@copyright: 2008-2009 MoinMoin:BastianBlank
+@license: GNU GPL, see COPYING for details.
+"""
+
+
+class RegistryBase(object):
+    PRIORITY_REALLY_FIRST = -20
+    PRIORITY_FIRST = -10
+    PRIORITY_MIDDLE = 0
+    PRIORITY_LAST = 10
+    PRIORITY_REALLY_LAST = 20
+
+    class _Entry(object):
+        def __init__(self, factory, priority):
+            self.factory, self.priority = factory, priority
+
+        def __cmp__(self, other):
+            if isinstance(other, self.__class__):
+                return cmp(self.factory, other.factory)
+            return cmp(self.factory, other)
+
+    def __init__(self):
+        self._converters = []
+
+    def _get(self, *args, **kw):
+        for entry in self._converters:
+            conv = entry.factory(*args, **kw)
+            if conv is not None:
+                return conv
+
+    def _sort(self):
+        self._converters.sort(key=lambda a: a.priority)
+
+    def get(self, *args, **kw):
+        raise NotImplementedError
+
+    def register(self, factory, priority=PRIORITY_MIDDLE):
+        if factory not in self._converters:
+            self._converters.append(self._Entry(factory, priority))
+            self._sort()
+
+    def unregister(self, factory):
+        self._converters.remove(factory)
+        self._sort()
