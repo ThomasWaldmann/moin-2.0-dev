cb124e7099d50f6132dff327fb19665f83d293d8
TW <tw@waldmann-edv.de>
2010-02-04 03:11:55 +0100

    update module header template

--- MoinMoin/_template.py
+++ MoinMoin/_template.py
@@ -1,10 +1,10 @@
-# -*- coding: iso-8859-1 -*-
+# -*- coding: ascii -*-
 """
     MoinMoin - <short description>
 
     <what this stuff does ... - verbose enough>
 
-    @copyright: 2007 MoinMoin:YourNameHere
+    @copyright: 2010 MoinMoin:YourNameHere
     @license: GNU GPL, see COPYING for details.
 """
 
