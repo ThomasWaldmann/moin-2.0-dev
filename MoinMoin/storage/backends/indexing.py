ec77cd440d7a803e3e7b85d9def0b6e1f1e3383f
TW <tw@waldmann-edv.de>
2010-05-16 21:10:19 +0200

    add dummy indexing middleware

--- MoinMoin/storage/backends/indexing.py
+++ MoinMoin/storage/backends/indexing.py
@@ -3,134 +3,78 @@
     MoinMoin - Indexing Middleware (IMW)
 
     This backend is a middleware implementing metadata (later maybe also data)
-    indexing. It does not store any data, but uses a given backend for this.
-    This middleware is injected between the ACL middleware and the actual
-    backend used for storage. It is independent of the backend being used.
+    indexing. It does not store any data, but uses a given target backend for
+    this. This middleware is injected directly above the real storage backend.
+    It is independent of the backend being used.
 
-    XXX is this needed: Instances of the IMW are bound to individual request objects.
-
-    The backend itself (and the objects it returns) need to be wrapped in order
-    to make sure that no object of the real backend is (directly or indirectly)
-    modified by the user of the API.
+    The target backend itself (and the items / revisions it returns) need to
+    be all proxied (wrapped in a proxy object) in order to make sure that no
+    object of the target backend is (directly or indirectly) modified by the
+    user of the API.
 
     @copyright: 2010 MoinMoin:ThomasWaldmann
     @license: GNU GPL, see COPYING for details.
 """
 
-from UserDict import DictMixin
+from objectproxy import Proxy
 
 from MoinMoin import log
 logging = log.getLogger(__name__)
 
 
-class IndexingWrapperBackend(object):
+class IndexingProxyBackend(Proxy):
     """
-    The IMW is bound to a specific request.
-    The actual backend is retrieved from the config upon request initialization.
-    Note: This may *not* inherit from MoinMoin.storage.Backend because that would
-    break our __getattr__ attribute 'redirects' (which are necessary because a backend
-    implementor may decide to use his own helper functions which the items and revisions
-    will still try to call).
+    Gives proxy items, not the target items.
     """
-    def __init__(self, request, backend):
-        """
-        @type request: MoinMoin request object
-        @param request: The request that the user issued.
-        @type backend: Some object that implements the storage API.
-        @param backend: The backend that we want to index.
-        """
-        self.request = request
-        self.backend = backend
-
-    def __getattr__(self, attr):
-        # Pass through any attribute lookup that is not subject to indexing.
-        return getattr(self.backend, attr)
-
     def create_item(self, itemname):
-        """
-        create new item and wrap for indexing
-        """
-        real_item = self.backend.create_item(itemname)
-        return IndexingWrapperItem(real_item, self)
+        s = super(self.__class__, self)
+        real_item = s.create_item(itemname)
+        return IndexingProxyItem(real_item, self)
 
     def get_item(self, itemname):
-        """
-        get existing item and wrap for indexing
-        """
-        real_item = self.backend.get_item(itemname)
-        return IndexingWrapperItem(real_item, self)
+        s = super(self.__class__, self)
+        real_item = s.get_item(itemname)
+        return IndexingProxyItem(real_item, self)
 
 
-class IndexingWrapperItem(object):
+class IndexingProxyItem(Proxy):
     """
-    Similar to IndexingWrapperBackend. Wraps a storage item for indexing.
-    """
-    def __init__(self, item, indexingbackend):
-        """
-        @type item: Object adhering to the storage item API.
-        @param item: The item we want to index.
-        @type indexingbackend: Instance of IndexingWrapperBackend.
-        @param indexingbackend: The IMW this item belongs to.
-        """
-        self.backend = indexingbackend
-        self.item = item
-        self._uncommitted_revision = None
+    Gives proxy revisions, not the real revisions.
 
-    def __getattr__(self, attr):
-        # Pass through any attribute lookup that is not subject to indexing.
-        return getattr(self.item, attr)
+    When a commit happens, index stuff.
+    """
+    def __init__(self, backend):
+        self.__backend = backend
+        self.__unindexed_revision = None
 
     def get_revision(self, revno):
-        """
-        get an existing (wrapped-for-indexing) revision
-        """
-        revision = self.item.get_revision(revno)
-        return IndexingWrapperRevision(self, revno, revision)
+        s = super(self.__class__, self)
+        real_revision = s.get_revision(revno)
+        return IndexingProxyRevision(real_revision, self)
 
     def create_revision(self, revno):
-        """
-        create a new (wrapped-for-indexing) revision
-        """
-        revision = self.item.create_revision(revno)
-        self._uncommitted_revision = IndexingWrapperRevision(self, revno, revision)
-        return self._uncommitted_revision
+        s = super(self.__class__, self)
+        real_revision = s.create_revision(revno)
+        self.__unindexed_revision = IndexingProxyRevision(real_revision, self)
+        return self.__unindexed_revision
 
     # TODO implement index update for item-level metadata
     def commit(self):
-        """
-        commit and index
-        """
-        self._uncommitted_revision.update_index()
-        self._uncommitted_revision = None
-        return self.item.commit()
+        self.__unindexed_revision.update_index()
+        self.__unindexed_revision = None
+        return super(self.__class__, self).commit()
 
     def rollback(self):
-        self._uncommitted_revision = None
+        self.__unindexed_revision = None
         return self.item.rollback()
 
 
-class IndexingWrapperRevision(DictMixin):
+class IndexingProxyRevision(Proxy):
     """
-    Wrapper for revision classes. We need to wrap NewRevisions because they allow altering data.
-    We need to wrap StoredRevisions since they offer a destroy() method and access to their item.
-    The caller should know what kind of revision he gets. Hence, we just implement the methods of
-    both, StoredRevision and NewRevision. If a method is invoked that is not defined on the
-    kind of revision we wrap, we will see an AttributeError one level deeper anyway, so this is ok.
+    Proxied Revision with indexing support in the Proxy.
     """
-    def __init__(self, item, revno, revision):
-        """
-        @type revision: Object adhering to the storage revision API.
-        @param revision: The revision we want to index.
-        @type item: Object adhering to the storage item API.
-        @param item: The item this revision belongs to
-        """
-        self.revision = revision
-        self._revno = revno # TODO: some code outside this class relies on ._revno
-        self.item = item
-
-    def __getattr__(self, attr):
-        # Pass through any attribute lookup that is not subject to indexing.
-        return getattr(self.revision, attr)
+    def __init__(self, item):
+        self.__item = item
 
     def update_index(self):
         """
