ab9d5eb2945a2db165b097d8b8fdd26c9fc4eb27
Diogenes Augusto <diofeher@gmail.com>
2010-05-27 12:26:56 -0300

    Adding templates structures and modifying JinjaTheme class with some method of ThemeBase

--- MoinMoin/theme/templating.py
+++ MoinMoin/theme/templating.py
@@ -1,21 +1,73 @@
 # -*- coding: iso-8859-1 -*-
-'''
+"""
     MoinMoin - Jinja2010 GSoC project
-    @copyright: 2003-2009 MoinMoin:ThomasWaldmann,
-                2008 MoinMoin:RadomirDopieralski,
-                2010 MoinMoin:DiogenesAugustoFernandesHermínio
-'''
+    
+    This module will be used to rendering a theme using Jinja2.
+    
+    @copyright: 2010 MoinMoin:DiogenesAugustoFernandesHermínio
+    @license: GNU GPL, see COPYING for details.
+"""
 
 from MoinMoin.support.jinja2 import Enviroment, FileSystemLoader
 
 TEMPLATES_DIR = '/templates/'
 ENVIROMENT = Enviroment(loader=FileSystemLoader(TEMPLATES_DIR))
 
-def render_jinja2(filename, context={}):
-    '''
-    Function that renders using Jinja2.
-    '''
-    template = ENVIROMENT.get_template(filename)
-    rendered = template.render(**context)
-    return rendered
+class JinjaTheme(object):
+    """
+    Class used to replace actual ThemeBase.
+    """
+    
+    def __init__(self, request):
+        """
+        Initialize the JinjaTheme object.
+
+        @param request: the request object
+        """
+        # Gonna use this for now, it came from ThemeBase
+        self.request = request
+        self.cfg = request.cfg
+        self._cache = {} # Used to cache elements that may be used several times
+        self._status = []
+        self._send_title_called = False
+
+        jinja_cachedir = os.path.join(request.cfg.cache_dir, 'jinja')
+        try:
+            os.mkdir(jinja_cachedir)
+        except:
+            pass
+
+        jinja_templatedir = os.path.join(os.path.dirname(__file__), '..', 'templates')
+
+        self.env = Environment(loader=FileSystemLoader(jinja_templatedir),
+                               bytecode_cache=FileSystemBytecodeCache(jinja_cachedir, '%s'),
+                               extensions=['jinja2.ext.i18n'])
+        from werkzeug import url_quote, url_encode
+        self.env.filters['urlencode'] = lambda x: url_encode(x)
+        self.env.filters['urlquote'] = lambda x: url_quote(x)
+        self.env.filters['datetime_format'] = lambda tm, u=request.user: u.getFormattedDateTime(tm)
+        self.env.filters['date_format'] = lambda tm, u=request.user: u.getFormattedDate(tm)
+        self.env.filters['user_format'] = lambda rev, request=request: \
+                                              user.get_printable_editor(request,
+                                                                        rev[EDIT_LOG_USERID],
+                                                                        rev[EDIT_LOG_ADDR],
+                                                                        rev[EDIT_LOG_HOSTNAME])
+
+    def render_header(context={}):
+        """
+        Function that renders header.
+        """
+        return render('header.html', **context)    
+    
+    def render_footer(context={}):
+        """
+        Function that renders footer.
+        """
+        return render('footer.html', **context)
+    
+    def render(filename, context={}):
+        """
+        Function that renders using Jinja2.
+        """
+        return template.render(**context)
     
