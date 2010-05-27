b558377e20451af9cb74783f686a704636fd0f22
Valentin Janiaut <valentin.janiaut@utbm.fr>
2010-05-27 22:59:21 +0200

    Add more tests for html_in

--- MoinMoin/converter2/_tests/test_html_in.py
+++ MoinMoin/converter2/_tests/test_html_in.py
@@ -21,18 +21,29 @@ class Base(object):
 
     output_re = re.compile(r'\s+xmlns(:\S+)?="[^"]+"')
 
-    def do(self, input, path, string_test, args={}):
+    def handle_input(self, input, args={}):
         out = self.conv(input, **args)
         f = StringIO.StringIO()
-        out.write(f.write, namespaces=self.namespaces,)
+        out.write(f.write, namespaces=self.namespaces, )
+        return self.output_re.sub(u'', f.getvalue())
 
-        str_input = self.output_re.sub(u'',f.getvalue())
-        tree = etree.parse(StringIO.StringIO(str_input))
-        
+
+    def do(self, input, path, text, args={}):
+        string_to_parse = self.handle_input(input)
+        tree = etree.parse(StringIO.StringIO(string_to_parse))
         r = tree.xpath(path)
 
         assert len(r) == 1
-        assert tree.xpath(path) == string_test
+        assert r[0].text == text
+
+    def do_with_attr(self, input, path, attr, attr_value, text, args={}):
+        string_to_parse = self.handle_input(input)
+        print string_to_parse
+        tree = etree.parse(StringIO.StringIO(string_to_parse))
+        r = tree.xpath(path)
+        assert len(r) == 1
+        assert r[0].get(attr) == attr_value
+        assert r[0].text == text
 
 class TestConverter(Base):
     def setup_class(self):
@@ -41,8 +52,45 @@ class TestConverter(Base):
     def test_base(self):
         data = [
             ('<div><p>Test</p></div>',
-             'body/p',
+             '/page/body/p',
              'Test'),
+            ('<div><h1>Test</h1></div>',
+            '/page/body/h',
+            'Test'),
+        ]
+        for i in data:
+            yield (self.do, ) + i
+
+    def test_title(self):
+        data = [
+            ('<div><h2>Test</h2></div>',
+              '/page/body/h',
+              'outline-level',
+              '2',
+              'Test'),
+            ('<div><h6>Test</h5></div>',
+              '/page/body/h',
+              'outline-level',
+              '6',
+              'Test'),
+        ]
+        for i in data:
+            yield (self.do_with_attr, ) + i
+
+    def test_basic_style(self):
+        data = [
+            ('<div><p><em>Test</em></p></div>',
+              '/page/body/p/emphasis',
+              'Test'),
+            ('<div><p><strong>Test</strong></p></div>',
+              '/page/body/p/strong',
+              'Test'),
+            ('<div><pre>Code</pre></div>',
+              '/page/body/blockcode',
+              'Code'),
+            ('<div><p><tt>Code</tt></p></div>',
+              '/page/body/p/code',
+              'Code'),
         ]
         for i in data:
             yield (self.do, ) + i
