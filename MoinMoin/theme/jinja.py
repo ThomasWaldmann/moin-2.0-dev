a4d2de0b5a93b0573d72a3f569ab79f48e09492e
Diogenes Augusto <diofeher@gmail.com>
2010-05-31 10:27:59 -0300

    changed variable in footer.html

--- MoinMoin/theme/jinja.py
+++ MoinMoin/theme/jinja.py
@@ -1209,9 +1209,6 @@ actionsMenuInit('%(label)s');
         output = []
         # later: <html xmlns=\"http://www.w3.org/1999/xhtml\">
         output.append("""\
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
 %s
 %s
 %s
