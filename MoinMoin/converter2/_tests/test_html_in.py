b2b68db527af39437b07de55ddbda49f7b7f5015
Valentin Janiaut <valentin.janiaut@utbm.fr>
2010-06-08 12:39:50 +0200

    Rewrite test_html_in to use boolean xpath query for a better genericity

--- MoinMoin/converter2/_tests/test_html_in.py
+++ MoinMoin/converter2/_tests/test_html_in.py
@@ -34,13 +34,13 @@ class Base(object):
         return self.output_re.sub(u'', f.getvalue())
 
 
-    def do(self, input, path, text, args={}):
+    def do(self, input, path, args={}):
         string_to_parse = self.handle_input(input)
         tree = etree.parse(StringIO.StringIO(string_to_parse))
-        r = tree.xpath(path, namespaces=self.namespaces_xpath)
+        assert (tree.xpath(path, namespaces=self.namespaces_xpath))
 
-        assert len(r) == 1
-        assert r[0].text == text
+        #assert len(r) == 1
+        #assert r[0].text == text
 
 class TestConverter(Base):
     def setup_class(self):
@@ -49,11 +49,9 @@ class TestConverter(Base):
     def test_base(self):
         data = [
             ('<html><div><p>Test</p></div></html>',
-             '/page/body/div/p',
-            'Test'),
+             '/page/body/div[p="Test"]'),
             ('<html><div><p>First paragraph</p><h1>Title</h1><p><em>Paragraph</em></p></div></html>',
-             '/page/body/div/p[2]/emphasis',
-             'Paragraph'), # This test need to be improved
+             '/page/body/div/p[2][emphasis="Paragraph"]'),
         ]
         for i in data:
             yield (self.do, ) + i
@@ -61,11 +59,9 @@ class TestConverter(Base):
     def test_title(self):
         data = [
             ('<html><h2>Test</h2></html>',
-              '/page/body/h[@outline-level=2]',
-              'Test'),
+              '/page/body/h[@outline-level=2] | /page/body[h="Test"]'),
             ('<html><h6>Test</h6></html>',
-              '/page/body/h[@outline-level=6]',
-              'Test'),
+              '/page/body/h[@outline-level=6] | /page/body[h="Test"]'),
         ]
         for i in data:
             yield (self.do, ) + i
@@ -73,14 +69,11 @@ class TestConverter(Base):
     def test_basic_style(self):
         data = [
             ('<html><p><em>Test</em></p></html>',
-              '/page/body/p/emphasis',
-              'Test'),
+              '/page/body/p[emphasis="Test"]'),
             ('<html><p><i>Test</i></p></html>',
-              '/page/body/p/emphasis',
-              'Test'),
+              '/page/body/p[emphasis="Test"]'),
             ('<html><p><strong>Test</strong></p></html>',
-              '/page/body/p/strong',
-              'Test'),
+              '/page/body/p[strong="Test"]'),
         ]
         for i in data:
             yield (self.do, ) + i
@@ -88,14 +81,11 @@ class TestConverter(Base):
     def test_span(self):
         data = [
             ('<html><p><sub>sub</sub>script</p></html>',
-             '/page/body/p/span[@base-line-shift="sub"]',
-             'sub'),
+             '/page/body/p/span[@base-line-shift="sub"] | /page/body/p[span="sub"]'),
             ('<html><p><sup>super</sup>script</p></html>',
-             '/page/body/p/span[@base-line-shift="super"]',
-             'super'),
+             '/page/body/p/span[@base-line-shift="super"] | /page/body/p[span="super"]'),
             ('<html><p><u>underline</u></html>',
-             '/page/body/p/span[@text-decoration="underline"]',
-             'underline'),
+             '/page/body/p/span[@text-decoration="underline"] | /page/body/p[span="underline"]'),
         ]
         for i in data:
             yield (self.do, ) + i
@@ -103,8 +93,7 @@ class TestConverter(Base):
     def test_link(self):
         data = [
             ('<html><p><a href="uri:test">Test</a></p></html>',
-              '/page/body/p/a[@xlink:href="uri:test"]',
-             'Test'),
+              '/page/body/p/a[@xlink:href="uri:test"] | /page/body/p[a="Test"]'),
         ]
         for i in data:
             yield (self.do, ) + i
@@ -112,17 +101,13 @@ class TestConverter(Base):
     def test_code(self):
         data = [
             ('<html><div><code>Code</code></div></html>',
-             '/page/body/div/code',
-             'Code'),
+             '/page/body/div[code="Code"]'),
             ('<html><div><samp>Code</samp></div></html>',
-             '/page/body/div/code',
-             'Code'),
+             '/page/body/div[code="Code"]'),
             ('<html><pre>Code</pre></html>',
-              '/page/body/blockcode',
-              'Code'),
+              '/page/body[blockcode="Code"]'),
             ('<html><p><tt>Code</tt></p></html>',
-              '/page/body/p/code',
-              'Code'),
+              '/page/body/p[code="Code"]'),
         ]
         for i in data:
             yield (self.do, ) + i
