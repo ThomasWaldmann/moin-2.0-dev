fc87c7fb5e03b2bbc57086ab3f963ab6084f91a7
Valentin Janiaut <valentin.janiaut@utbm.fr>
2010-06-29 16:38:02 +0200

    Add tests for a bug with newline character inside a list

--- MoinMoin/converter2/_tests/test_html_in_out.py
+++ MoinMoin/converter2/_tests/test_html_in_out.py
@@ -175,6 +175,9 @@ class TestConverter(Base):
               '/div/div/ul[li="Item"]'),
             ('<div><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul></div>',
              '/div/ul[li[1]="Item 1"][li[2]="Item 2"][li[3]="Item 3"]'),
+             #Test for bug with line return and spaces
+            ('<div><ul><li>\n Item 1</li>\n<li>\n Item 2</li>\n<li>\n Item 3</li>\n</ul></div>',
+             '/div/ul[li[1]="Item 1"][li[2]="Item 2"][li[3]="Item 3"]'),
         ]
         for i in data:
             yield (self.do, ) + i
