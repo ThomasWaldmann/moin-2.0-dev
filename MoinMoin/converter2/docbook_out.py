9ced3acb9c9d0e8a5f21d3c43957bba671a31a86
Valentin Janiaut <valentin.janiaut@utbm.fr>
2010-07-01 22:41:07 +0200

    Add docbook_out converter groundwork

---
+++ MoinMoin/converter2/docbook_out.py
@@ -0,0 +1,104 @@
+"""
+MoinMoin - DocBook output converter
+
+Converts an internal document tree into a DocBook v.5 tree.
+
+@copyright: 2010 MoinMoin:ValentinJaniaut
+@license: GNU GPL, see COPYING for details.
+"""
+
+from __future__ import absolute_import
+
+from emeraldtree import ElementTree as ET
+
+from MoinMoin import wikiutil
+from MoinMoin.util.tree import html, moin_page, xlink, docbook
+
+class Converter(object):
+    """
+    Converter application/x.moin.document -> application/docbook+xml
+    """
+
+    namespaces_visit = {
+        moin_page: 'moinpage'
+    }
+
+    def __call__(self, element):
+        return self.visit(element)
+
+    def do_children(self, element):
+        new = []
+        for child in element:
+            if isinstance(child, ET.Element):
+                r = self.visit(child)
+                if r is None:
+                    r = ()
+                elif not isinstance(r, (list, tuple)):
+                    r = (r, )
+                new.extend(r)
+            else:
+                new.append(child)
+        return new
+
+    def new(self, tag, attrib, children):
+        """
+        Return a new element in the DocBook tree
+        """
+        return ET.Element(tag, attrib=attrib, children=children)
+
+    def new_copy(self, tag, element, attrib):
+        """
+        Function to copy one element to the DocBook tree
+
+        It first converts the children of the element,
+        and then the element itself
+        """
+
+        children = self.do_children(element)
+        return self.new(tag, attrib, children)
+
+    def visit(self, element):
+        """
+        Function called at each element, to process it.
+
+        It will just determine the namespace of our element,
+        then call a dedicated function to handle conversion
+        for the found namespace.
+        """
+        uri = element.tag.uri
+        name = self.namespaces_visit.get(uri, None)
+        if name is not None:
+            method_name = 'visit_' + name
+            method = getattr(self, method_name, None)
+            if method is not None:
+                return method(element)
+        # We process children of the unknown element
+        return self.do_children(element)
+
+    def visit_moinpage(self, element):
+        """
+        Function called to handle the conversion of elements
+        belonging to the moin_page namespace.
+
+        We will choose the most appropriate procedure to convert
+        the element according to his name
+        """
+
+        method_name = 'visit_moinpage_' + element.tag.name.replace('-', '_')
+        method = getattr(self, method_name, None)
+        if method:
+            return method(element)
+
+        # Otherwise we process the children of the unknown element
+        return self.do_children(element)
+
+    def visit_moinpage_page(self, elem):
+        for item in elem:
+            if item.tag.uri == moin_page and item.tag.name == 'body':
+                return self.new_copy(docbook.article, item, attrib={})
+
+        raise RuntimeError('page:page need to contain exactly one page body tag, got %r' % elem[:])
+
+    def visit_moinpage_p(self, element):
+        return self.new_copy(docbook.para, element, attrib={})
+
