6e4d1ddc2a6cc72d2796edfe24b6f8c70c3880b9
Valentin Janiaut <valentin.janiaut@utbm.fr>
2010-07-02 15:30:41 +0200

    Add automatically docbook namespace for the DocBook_IN test, and fix wrong docbook input

--- MoinMoin/converter2/_tests/test_docbook_in.py
+++ MoinMoin/converter2/_tests/test_docbook_in.py
@@ -20,22 +20,30 @@ logging = log.getLogger(__name__)
 import StringIO
 
 class Base(object):
-    namespaces = {
+    input_namespaces = ns_all = 'xmlns="%s"' % (
+        docbook.namespace)
+
+    output_namespaces = {
         moin_page.namespace: '',
         xlink.namespace: 'xlink',
     }
 
+    input_re = re.compile(r'^(<[a-z:]+)')
     output_re = re.compile(r'\s+xmlns="[^"]+"')
 
-    def handle_input(self, input, args):
-        out = self.conv(input, **args)
+    def handle_input(self, input):
+        return self.input_re.sub(r'\1 ' + self.input_namespaces, input)
+
+    def handle_output(self, input, args):
+        to_conv = self.handle_input(input)
+        out = self.conv(to_conv, **args)
         f = StringIO.StringIO()
-        out.write(f.write, namespaces=self.namespaces, )
+        out.write(f.write, namespaces=self.output_namespaces, )
         return self.output_re.sub(u'', f.getvalue())
 
 
     def do(self, input, xpath_query):
-        string_to_parse = self.handle_input(input, args={})
+        string_to_parse = self.handle_output(input, args={})
         logging.debug("After the DOCBOOK_IN conversion : %s" % string_to_parse)
         tree = etree.parse(StringIO.StringIO(string_to_parse))
         assert (tree.xpath(xpath_query))
@@ -46,7 +54,7 @@ class TestConverter(Base):
 
     def test_base(self):
         data = [
-            ('<article><para>Test</para>',
+            ('<article><para>Test</para></article>',
              '/page/body[p="Test"]'),
             ('<article><sect1><title>Heading 1</title> <para>First Paragraph</para></sect1></article>',
              '/page/body[./h[@outline-level="1"][text()="Heading 1"]][./p[text()="First Paragraph"]]'),
