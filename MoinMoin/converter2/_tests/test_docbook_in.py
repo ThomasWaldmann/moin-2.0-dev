d85a93c85930ba3443fd094e79c39cb68088a39a
Valentin Janiaut <valentin.janiaut@utbm.fr>
2010-07-02 13:02:50 +0200

    Add first tests for the DocBook_in converter

---
+++ MoinMoin/converter2/_tests/test_docbook_in.py
@@ -0,0 +1,64 @@
+"""
+MoinMoin - Tests for MoinMoin.converter2.docbook_in
+
+@copyright: 2010 MoinMoin:ValentinJaniaut
+@license: GNU GPL, see COPYING for details.
+"""
+
+import py.test
+import re
+
+try:
+    from lxml import etree
+except:
+    py.test.skip("lxml module required to run test for docbook_in converter.")
+
+from MoinMoin.converter2.docbook_in import *
+from emeraldtree.tree import *
+from MoinMoin import log
+logging = log.getLogger(__name__)
+import StringIO
+
+class Base(object):
+    namespaces = {
+        moin_page.namespace: '',
+        xlink.namespace: 'xlink',
+    }
+
+    output_re = re.compile(r'\s+xmlns="[^"]+"')
+
+    def handle_input(self, input, args):
+        out = self.conv(input, **args)
+        f = StringIO.StringIO()
+        out.write(f.write, namespaces=self.namespaces, )
+        return self.output_re.sub(u'', f.getvalue())
+
+
+    def do(self, input, xpath_query):
+        string_to_parse = self.handle_input(input, args={})
+        logging.debug("After the DOCBOOK_IN conversion : %s" % string_to_parse)
+        tree = etree.parse(StringIO.StringIO(string_to_parse))
+        assert (tree.xpath(xpath_query))
+
+class TestConverter(Base):
+    def setup_class(self):
+        self.conv = Converter()
+
+    def test_base(self):
+        data = [
+            ('<article><para>Test</para>',
+             '/page/body[p="Test"]'),
+            ('<article><sect1><title>Heading 1</title> <para>First Paragraph</para></sect1></article>',
+             '/page/body[./h[@outline-level="1"][text()="Heading 1"]][./p[text()="First Paragraph"]]'),
+        ]
+        for i in data:
+            yield (self.do, ) + i
+
+    def test_title(self):
+        data = [
+            ('<article><sect1><title>Heading 1</title> <para>First</para><sect2><title>Heading 2</title><para>Second</para></sect2></sect1></article>',
+             '/page/body[./h[@outline-level="1"][text()="Heading 1"]][./p[@text()="First"]][./h[@outline-level="2"][text()="Heading 2"]][./p[@text()="Second"]]'),
+        ]
+
+        for i in data:
+            yield (self.do, ) + i
