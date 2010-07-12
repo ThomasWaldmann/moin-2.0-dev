e5f364c06864d542ef15c106569138bf70b62c4b
Diogenes Augusto <diofeher@gmail.com>
2010-07-12 20:25:06 -0300

    [templating] covered more possibilities in navibar tests

--- MoinMoin/theme/_tests/test_navi_bar.py
+++ MoinMoin/theme/_tests/test_navi_bar.py
@@ -13,8 +13,44 @@ class TestNaviBar(object):
     def setup_method(self, method):
         self.theme = ThemeBase(self.request)
 
+    def test_itemname(self):
+        item_name, url, link_text, interwiki = self.theme.splitNavilink('ItemName')
+        assert link_text == 'ItemName'
+        assert url == '/ItemName'
+        assert interwiki == ''
+        
+    def test_itemname_with_text(self):
+        item_name, url, link_text, interwiki = self.theme.splitNavilink('[[ItemName|LinkText]]')
+        assert link_text == 'LinkText'
+        assert url == '/ItemName'
+        assert interwiki == ''
+        
     def test_interwiki(self):
-        item_name, url, link_text, title = self.theme.splitNavilink('MoinMoin:ItemName')
+        item_name, url, link_text, interwiki = self.theme.splitNavilink('MoinMoin:ItemName')
         assert link_text == 'ItemName'
         assert url == 'http://moinmo.in/ItemName'
-        assert title == 'MoinMoin'
+        assert interwiki == 'MoinMoin'
+
+    def test_interwiki_with_text(self):
+        item_name, url, link_text, interwiki = self.theme.splitNavilink('[[MoinMoin:ItemName|LinkText]]')
+        assert link_text == 'LinkText'
+        assert url == 'http://moinmo.in/ItemName'
+        assert interwiki == 'MoinMoin'
+
+    def test_wiki_interwiki_with_text(self):
+        item_name, url, link_text, interwiki = self.theme.splitNavilink('[[wiki:MoinMoin:ItemName|LinkText]]')
+        assert link_text == 'LinkText'
+        assert url == 'http://moinmo.in/ItemName'
+        assert interwiki == 'MoinMoin'
+
+    def test_external_url(self):
+        item_name, url, link_text, interwiki = self.theme.splitNavilink('http://diofeher.net/')
+        assert link_text == 'http://diofeher.net/'
+        assert url == 'http://diofeher.net/'
+        assert interwiki == ''
+
+    def test_external_url_with_title(self):
+        item_name, url, link_text, interwiki = self.theme.splitNavilink('[[http://google.com/|Google]]')
+        assert link_text == 'Google'
+        assert url == 'http://google.com/'
+        assert interwiki == ''
