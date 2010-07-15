ca382f9b050536be1e2e3ed6c324581b77e57ebd
Valentin Janiaut <valentin.janiaut@utbm.fr>
2010-07-15 18:45:25 +0200

    Add test for <blockcode> conversion for DocBook_OUT converter

--- MoinMoin/converter2/_tests/test_docbook_out.py
+++ MoinMoin/converter2/_tests/test_docbook_out.py
@@ -122,6 +122,9 @@ class TestConverter(Base):
             # Link conversion
             ('<page><body><p><a xlink:href="uri:test" xlink:title="title">link</a></p></body></page>',
               '/article/para/link[@xlink:href="uri:test"][@xlink:title="title"][text()="link"]'),
+            # Blockcode conversion into <screen>
+            ('<page><body><blockcode>Text</blockcode></body></page>',
+             '/article[screen="Text"]'),
         ]
         for i in data:
             yield (self.do, ) + i
