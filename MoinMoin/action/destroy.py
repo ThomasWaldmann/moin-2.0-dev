b32670c7215a4b552be1ea2690a64e7a59a5a1ea
Diogenes Augusto Fernandes Herminio <diofeher@gmail.com>
2010-07-16 14:01:11 -0300

    [templating] adapting destroy to return unicode

--- MoinMoin/action/destroy.py
+++ MoinMoin/action/destroy.py
@@ -18,7 +18,7 @@ def execute(item_name, request):
     item = Item.create(request, item_name, rev_no=request.rev)
     if request.method == 'GET':
         content = item.do_destroy()
-        request.theme.render_content(item_name, content)
+        return content
     elif request.method == 'POST':
         cancelled = 'button_cancel' in request.form
         if not cancelled:
