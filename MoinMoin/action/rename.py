889d785988cd6d062e99a8786daf0c878755c8af
Diogenes Augusto Fernandes Herminio <diofeher@gmail.com>
2010-07-18 12:53:13 -0300

    [templating] refactored rename action

--- MoinMoin/action/rename.py
+++ MoinMoin/action/rename.py
@@ -13,8 +13,7 @@ def execute(item_name, request):
     item = Item.create(request, item_name)
     if request.method == 'GET':
         content = item.do_rename()
-        request.headers.add('Content-Type', 'text/html; charset=utf-8')
-        request.write(content)
+        return content
     elif request.method == 'POST':
         cancelled = 'button_cancel' in request.form
         if not cancelled:
