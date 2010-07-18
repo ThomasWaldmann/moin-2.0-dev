2c86795bcbd190acdd7ad6d09c3ff28c2473ebd9
Diogenes Augusto Fernandes Herminio <diofeher@gmail.com>
2010-07-18 12:43:11 -0300

    [templating] changed (c) in highlight action

--- MoinMoin/action/highlight.py
+++ MoinMoin/action/highlight.py
@@ -3,7 +3,8 @@
     MoinMoin - Action to save item content to a file download
 
     @copyright: 2009 MoinMoin:ThomasWaldmann,
-                2010 MoinMoin:ReimarBauer
+                2010 MoinMoin:ReimarBauer,
+                2010 MoinMoin:DiogenesAugusto
 
     @license: GNU GPL, see COPYING for details.
 """
