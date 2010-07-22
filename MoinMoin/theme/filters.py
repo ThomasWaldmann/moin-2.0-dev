02c452d1ec12fcd85832b8bd88f360592715f30e
TW <tw@waldmann-edv.de>
2010-07-22 23:17:25 +0100

    fix failing pep8 and html tests

--- MoinMoin/theme/filters.py
+++ MoinMoin/theme/filters.py
@@ -9,11 +9,11 @@
 def shorten_item_name(name, length=25):
     """
     Shorten item names
-    
+
     Shorten very long item names that tend to break the user
     interface. The short name is usually fine, unless really stupid
     long names are used (WYGIWYD).
-    
+
     @param name: item name, unicode
     @param length: maximum length for shortened item names, int
     @rtype: unicode
