ff6ec345894b5585e3c892143d56e71814b60d2c
TW <tw@waldmann-edv.de>
2010-07-29 23:11:48 +0200

    smiley converter: use url_for(), use smiley list from converter, cosmetic changes

--- MoinMoin/converter2/smiley.py
+++ MoinMoin/converter2/smiley.py
@@ -12,22 +12,17 @@ import re
 
 from emeraldtree import ElementTree as ET
 
-from MoinMoin import config
+from flask import url_for
+
 from MoinMoin.util.iri import Iri
-from MoinMoin.config import url_prefix_static
 from MoinMoin.util.tree import moin_page, xlink
 
 class Converter(object):
     """
     Replace each smiley by the corresponding <object> in the DOM Tree
     """
-
-    # Data for the smiley
-    # TODO: Retrieve the smiley list from the theme.
-
     smileys = {
-        # We cannot use raw string, because raw string cannot
-        # end with a backslash
+        # markup: (image filename, w, h)
         'X-(': ('angry.png', 16, 16),
         ':D': ('biggrin.png', 16, 16),
         '<:(': ('frown.png', 16, 16),
@@ -61,19 +56,16 @@ class Converter(object):
         '{o}': ('star_off.png', 16, 16),
     }
 
-    # We use the smiley from the config file currently
-    # TODO: Maybe we only use the information from the theme
     smiley_rule = ur"""
     (^|(?<=\s))  # we require either beginning of line or some space before a smiley
     (%(smiley)s)  # one of the smileys
     (?=\s)  # we require some space after the smiley
-""" % {'smiley': u'|'.join([re.escape(s) for s in config.smileys])}
+""" % {'smiley': u'|'.join([re.escape(s) for s in smileys])}
+
+    smiley_re = re.compile(smiley_rule, re.UNICODE|re.VERBOSE)
 
     # We do not process any smiley conversion within these elements.
-    tag_to_ignore = set(['code', 'blockcode'])
-
-    # The compiled regular expression
-    smiley_re = re.compile(smiley_rule, re.UNICODE|re.VERBOSE)
+    tags_to_ignore = set(['code', 'blockcode', ])
 
     @classmethod
     def _factory(cls, input, output, icon=None, **kw):
@@ -85,7 +77,7 @@ class Converter(object):
         return content
 
     def do_children(self, element):
-        if element.tag.name in self.tag_to_ignore:
+        if element.tag.name in self.tags_to_ignore:
             return
         for child in element:
             if isinstance(child, ET.Element):
@@ -98,7 +90,7 @@ class Converter(object):
 
     def do_smiley(self, element):
         """
-        From a text, return a list with smiley replaced
+        From a text, return a list with smileys replaced
         by object elements, and the former text for the
         other element of the list.
         """
@@ -121,7 +113,7 @@ class Converter(object):
             key = xlink.href
             # TODO: Retrieve the name of the theme used by the user
             #        to get the correct smiley image
-            attrib[key] = Iri("%s/modernized/img/smileys/%s" % (url_prefix_static, icon))
+            attrib[key] = Iri(url_for('static', filename="modernized/img/smileys/%s" % icon))
             return ET.Element(moin_page('object'), attrib=attrib, children={})
         return text
 
