64bda7a88f3bf4549991166dd2b45cff854b4eed
Dmitry Andreev <klieg.polar@gmail.com>
2010-08-03 21:51:32 +0400

    Mediawiki->DOM: more tests

--- MoinMoin/converter2/_tests/test_mediawiki_in.py
+++ MoinMoin/converter2/_tests/test_mediawiki_in.py
@@ -1,5 +1,5 @@
 """
-MoinMoin - Tests for MoinMoin.converter2.rst_in
+MoinMoin - Tests for MoinMoin.converter2.mediawiki_in
 
 @copyright: 2008 MoinMoin:BastianBlank
             2010 MoinMoin:DmitryAndreev
@@ -9,7 +9,7 @@ MoinMoin - Tests for MoinMoin.converter2.rst_in
 import py.test
 import re
 
-from MoinMoin.converter2.mediawiki_in_parser import *
+from MoinMoin.converter2.mediawiki_in import *
 
 
 class TestConverter(object):
@@ -25,51 +25,56 @@ class TestConverter(object):
 
     def test_base(self):
         data = [
-            (u"''italic''",
-                ''),
-            (u"Text\nTest",
-                ''),
-            (u'Text\n\nTest',
-                ''),
-            (u"'''bold'''", ''),
-            (u"'''''bold & italic'''''", ''),
-            (u"<nowiki>no ''markup''</nowiki>", ''),
-            (u'<u>underscore</u>', ''),
-            (u'<del>Strikethrough</del>', ''),
-            (u"<tt>Fixed width text</tt> or <code>source code</code>", ""),
-            (u"test <sup>super</sup> or <sub>sub</sub>", ""),
-            (u"text <blockquote> quote quote quote quote quote quote </blockquote> text", ""),
+            (u"''italic''", u'<page><body><p><emphasis>italic</emphasis></p></body></page>'),
+            (u"Text\nTest", u'<page><body><p>Text\nTest</p></body></page>'),
+            (u'Text\n\nTest', u'<page><body><p>Text</p><p>Test</p></body></page>'),
+            (u"'''bold'''", u'<page><body><p><strong>bold</strong></p></body></page>'),
+            (u"'''''bold and italic'''''", u'<page><body><p><strong><emphasis>bold and italic</emphasis></strong></p></body></page>'),
+            (u"<nowiki>no ''markup''</nowiki>\n\n<code>no ''markup''</code>\n\n<tt>no ''markup''</tt>", "<page><body><p><code>no ''markup''</code></p><p><code>no ''markup''</code></p><p><code>no ''markup''</code></p></body></page>"),
+            (u"<pre>no ''markup'' block</pre>", u"<page><body><p><blockcode>no ''markup'' block</blockcode></p></body></page>"),
+            (u'<u>underscore</u>', u'<page><body><p><span text-decoration="underline">underscore</span></p></body></page>'),
+            (u'<del>Strikethrough</del>', u'<page><body><p><span text-decoration="line-through">Strikethrough</span></p></body></page>'),
+            (u"test <sup>super</sup> or <sub>sub</sub>", u'<page><body><p>test <span baseline-shift="super">super</span> or <span baseline-shift="sub">sub</span></p></body></page>'),
+            (u"text <blockquote> quote quote quote quote quote quote </blockquote> text", u""),
+            (u"aaa<br />bbb", u"<page><body><p>aaa<line-break />bbb</p></body></page>"),
             (u"""=level 1=
-
 == level 2 ==
 ===level 3===
 ====level 4====
 =====level 5=====
 ======level 6======
-""", ""),
+""", u'<page><body><h outline-level="1">level 1</h><h outline-level="2">level 2</h><h outline-level="3">level 3</h><h outline-level="4">level 4</h><h outline-level="5">level 5</h><h outline-level="6">level 6</h></body></page>')
+        ]
+        for i in data:
+            yield (self.do, ) + i
+
+    def test_list(self):
+        data = [
             (u"""* one
 * two
 * three
 ** three point one
 ** three point two
-""", ""),
+""", u'<page><body><list item-label-generate="unordered"><list-item><list-item-body><p>one</p></list-item-body></list-item><list-item><list-item-body><p>two</p></list-item-body></list-item><list-item><list-item-body><p>three</p><list item-label-generate="unordered"><list-item><list-item-body><p>three point one</p></list-item-body></list-item><list-item><list-item-body><p>three point two</p></list-item-body></list-item></list></list-item-body></list-item></list></body></page>'),
             (u"""# one
 # two<br />spanning more lines<br />doesn't break numbering
 # three
 ## three point one
 ## three point two
-""", ""),
+# 4
+no point
+""", u'''<page><body><list item-label-generate="ordered"><list-item><list-item-body><p>one</p></list-item-body></list-item><list-item><list-item-body><p>two<line-break />spanning more lines<line-break />doesn't break numbering</p></list-item-body></list-item><list-item><list-item-body><p>three</p><list item-label-generate="ordered"><list-item><list-item-body><p>three point one</p></list-item-body></list-item><list-item><list-item-body><p>three point two</p></list-item-body></list-item></list></list-item-body></list-item><list-item><list-item-body><p>4</p></list-item-body></list-item></list><p>no point</p></body></page>'''),
             (""";item 1
 : definition 1
 ;item 2
 : definition 2-1
 : definition 2-2
 """, ""),
-            (";aaa : bbb", ""),
-            (""": Single indent
+            (u";aaa : bbb", u"<page><body><list><list-item><list-item-label>aaa </list-item-label><list-item-body><p> bbb</p></list-item-body></list-item></list></body></page>"),
+            (u""": Single indent
 :: Double indent
 ::::: Multiple indent
-""", ""),
+""", u'<page><body><list item-label-generate="None"><list-item><list-item-body><p>Single indent</p><list item-label-generate="None"><list-item><list-item-body><p>Double indent</p><list item-label-generate="None"><list-item><list-item-body><p>Multiple indent</p></list-item-body></list-item></list></list-item-body></list-item></list></list-item-body></list-item></list></body></page>'),
             ("""# one
 # two
 #* two point one
@@ -77,8 +82,13 @@ class TestConverter(object):
 # three
 #; three item one
 #: three def one
-""", ""),
-            (u"aaa<br />bbb", ""),
+""", "")
+        ]
+        for i in data:
+            yield (self.do, ) + i
+
+    def test_table(self):
+        data = [
             (u"""{|
 |Orange
 |Apple
@@ -89,8 +99,17 @@ class TestConverter(object):
 |Butter
 |Ice cream
 |}
-""", ""),
-
+""", u"<page><body><table><table-body><table-row><table-cell>Orange</table-cell><table-cell>Apple</table-cell></table-row><table-row><table-cell>Bread</table-cell><table-cell>Pie</table-cell></table-row><table-row><table-cell>Butter</table-cell><table-cell>Ice cream</table-cell></table-row></table-body></table></body></page>"),
+            (u"""{|style="border-width: 1px;"
+|style="border-style: solid; border-width: 1px" colspan="2"| Orange
+Apple
+|-
+|rowspan='2'| Bread
+|Pie
+|-
+|test
+|}
+""", u'<page><body><table style="border-width: 1px;"><table-body><table-row><table-cell number-columns-spanned="2" style="border-style: solid; border-width: 1px">Orange\nApple</table-cell></table-row><table-row><table-cell number-rows-spanned="2">Bread</table-cell><table-cell>Pie</table-cell></table-row><table-row><table-cell>test</table-cell></table-row></table-body></table></body></page>'),
         ]
         for i in data:
             yield (self.do, ) + i
@@ -103,6 +122,5 @@ class TestConverter(object):
 
     def do(self, input, output, args={}, skip=None):
         out = self.conv(input.split(u'\n'), **args)
-        print self.serialize(out) # delete this
         assert self.serialize(out) == output
 
