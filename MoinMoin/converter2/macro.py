80615036b33b1bb1783b2f71b2a4c17f2f4e064c
TW <tw@waldmann-edv.de>
2010-08-08 21:13:30 +0200

    macro converter: remove support for old macros, only macro2 style is supported now

--- MoinMoin/converter2/macro.py
+++ MoinMoin/converter2/macro.py
@@ -20,33 +20,6 @@ from MoinMoin.util import iri
 from MoinMoin.util.mime import type_moin_document
 from MoinMoin.util.tree import html, moin_page
 
-class _PseudoParser(object):
-    def __init__(self, request):
-        self.request = request
-        self.form = request.form
-
-class _PseudoRequest(object):
-    def __init__(self, request, name):
-        self.__request, self.__name = request, name
-        self.__written = False
-
-    def __getattr__(self, name):
-        return getattr(self.__request, name)
-
-    def write(self, *text):
-        text = ''.join((i.encode('ascii', 'replace') for i in text))
-        if text:
-            text.replace('\n', ' ')
-            if len(text) > 100:
-                text = text[:100] + '...'
-            from warnings import warn
-            message = 'Macro ' + self.__name + ' used request.write: ' + text
-            warn(message, DeprecationWarning, stacklevel=2)
-            self.__written = True
-
-    @property
-    def written(self):
-        return self.__written
 
 class Converter(object):
     @classmethod
@@ -87,19 +60,7 @@ class Converter(object):
         elem_body = context_block and moin_page.body() or moin_page.inline_body()
         elem_error = moin_page.error()
 
-        if not self._handle_macro_new(elem_body, elem_error, page, name, args, context_block, alt):
-            self._handle_macro_old(elem_body, elem_error, page, name, args, context_block, alt)
-
-        if len(elem_body):
-            elem.append(elem_body)
-        if len(elem_error):
-            elem.append(elem_error)
-
-    def _handle_macro_new(self, elem_body, elem_error, page, name, args, context_block, alt):
-        try:
-            cls = wikiutil.importPlugin(self.request.cfg, 'macro2', name, function='Macro')
-        except wikiutil.PluginMissingError:
-            return False
+        cls = wikiutil.importPlugin(self.request.cfg, 'macro2', name, function='Macro')
 
         try:
             macro = cls(self.request)
@@ -118,71 +79,10 @@ class Converter(object):
                     'error_msg': unicode(e),
                 })
 
-        return True
-
-    def _handle_macro_old(self, elem_body, elem_error, page, name, args, context, alt):
-        Formatter = wikiutil.searchAndImportPlugin(self.request.cfg, "formatter", 'compatibility')
-
-        request = _PseudoRequest(self.request, name)
-        page = Page.Page(request, unicode(page.path)[1:])
-        request.formatter = formatter = Formatter(request, page)
-
-        m = macro.Macro(_PseudoParser(request))
-
-        try:
-            ret = m.execute(name, args or None)
-        except ImportError, err:
-            message = unicode(err)
-            if not name in message:
-                raise
-            elem_error.append(message)
-            return
-        except AssertionError, e:
-            from warnings import warn
-            message = 'Macro ' + name + ' get an assertion in the compatibility formatter'
-            if e.args:
-                message += ': ' + e.args[0]
-            warn(message, DeprecationWarning)
-            ret = True
-        except NotImplementedError, e:
-            # Force usage of fallback on not implemented methods
-            from warnings import warn
-            message = 'Macro ' + name + ' calls methods in the compatibility formatter which are not implemented'
-            if e.args:
-                message += ': ' + e.args[0]
-            warn(message, DeprecationWarning)
-            ret = True
-        except Exception, e:
-            # we do not want that a faulty macro aborts rendering of the page
-            # and makes the wiki UI unusable (by emitting a Server Error),
-            # thus, in case of exceptions, we just log the problem and return
-            # some standard text.
-            logger.exception("Macro %s raised an exception:" % name)
-            _ = self.request.getText
-            elem_error.append(_('<<%(macro_name)s: execution failed [%(error_msg)s] (see also the log)>>') % {
-                    'macro_name': name,
-                    'error_msg': unicode(e),
-                })
-            return
-
-        if request.written:
-            message = 'Macro ' + name + ' used request.write'
-            elem_error.append(message)
-            return
-
-        if ret:
-            # Fallback to HTML formatter
-            HtmlFormatter = wikiutil.searchAndImportPlugin(self.request.cfg, "formatter", 'text/html')
-            request.formatter = m.formatter = HtmlFormatter(request)
-            m.formatter.setPage(page)
-
-            ret = m.execute(name, args or None)
-
-            # Pipe the result through the HTML parser
-            formatter = Formatter(request, page)
-            formatter.rawHTML(ret)
-
-        elem_body.extend(formatter.root[:])
+        if len(elem_body):
+            elem.append(elem_body)
+        if len(elem_error):
+            elem.append(elem_error)
 
     def recurse(self, elem, page):
         new_page_href = elem.get(moin_page.page_href)
