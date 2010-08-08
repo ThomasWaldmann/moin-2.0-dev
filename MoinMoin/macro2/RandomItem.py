b24285b40acb3401fe7f12e3ce08cf44b7a259bf
TW <tw@waldmann-edv.de>
2010-08-08 22:58:14 +0200

    fixed RandomItem macro

--- MoinMoin/macro2/RandomItem.py
+++ MoinMoin/macro2/RandomItem.py
@@ -1,62 +1,62 @@
 # -*- coding: iso-8859-1 -*-
 """
-    MoinMoin - RandomPage Macro displays one or multiple random page links
+    MoinMoin - RandomItem Macro displays one or multiple random item links.
 
-    Note: This macro behaves a bit different now as it always returns a span
-          with one (or multiple) random page links (separated by commas, if
-          needed).
-          In moin <= 1.7, the macro either emitted a single (inline) random
-          page link, or if multiple random pages were requested, it emitted
-          a (block) bullet list of random page links.
+    TODO: add mimetype param and only show items matching this mimetype
 
     @copyright: 2000 Juergen Hermann <jh@web.de>,
-                2008 MoinMoin:ThomasWaldmann
+                2008-2010 MoinMoin:ThomasWaldmann
     @license: GNU GPL, see COPYING for details.
 """
 
 import random
 random.seed()
 
-from MoinMoin.util import iri
+from MoinMoin.util.iri import Iri
 from MoinMoin.util.tree import moin_page, xlink
-from MoinMoin.Page import Page
+from MoinMoin.items import Item, AccessDeniedError
 from MoinMoin.macro2._base import MacroInlineBase
 
+
 class Macro(MacroInlineBase):
-    def macro(self, links=1):
+    def macro(self, content, arguments, page_url, alternative):
         request = self.request
-        links = max(links, 1) # at least 1 link
 
-        # Get full page unfiltered page list - very fast!
-        all_pages = list(request.rootpage.getPageList(user='', exists=0))
+        if arguments:
+            item_count = int(arguments[0])
+        else:
+            item_count = 1
 
-        # Now select random page from the full list, and if it exists and we
-        # can read it, save.
-        pages = []
+        all_item_names = [i.name for i in request.rootitem.list_items()]
+
+        # Now select random item from the full list, and if it exists and
+        # we can read it, save.
+        random_item_names = []
         found = 0
-        while found < links and all_pages:
-            # Take one random page from the list
-            pagename = random.choice(all_pages)
-            all_pages.remove(pagename)
+        while found < item_count and all_item_names:
+            # Take one random item from the list
+            item_name = random.choice(all_item_names)
+            all_item_names.remove(item_name)
 
-            # Filter out deleted pages or pages the user may not read.
-            page = Page(request, pagename)
-            if page.exists() and request.user.may.read(pagename):
-                pages.append(pagename)
+            # Filter out items the user may not read.
+            try:
+                item = Item.create(request, item_name)
+                random_item_names.append(item_name)
                 found += 1
+            except AccessDeniedError:
+                pass
 
-        if not pages:
-            return ''
+        if not random_item_names:
+            return
 
-        pages.sort()
+        random_item_names.sort()
 
         result = moin_page.span()
-        for name in pages:
-            link = unicode(iri.Iri(scheme=u'wiki', authority=u'', path=u'/' + name))
+        for name in random_item_names:
+            link = unicode(Iri(scheme=u'wiki', authority=u'', path=u'/' + name))
             result.append(moin_page.a(attrib={xlink.href: link}, children=[name]))
             result.append(", ")
 
         del result[-1] # kill last comma
         return result
-
 
