16a9c689d1306910ebd8ebf6fda4f1cea4c19dfb
TW <tw@waldmann-edv.de>
2010-08-09 11:14:14 +0200

    change page_front_page to 'Home'

--- MoinMoin/config/multiconfig.py
+++ MoinMoin/config/multiconfig.py
@@ -809,8 +809,8 @@ options_no_group_name = {
   )),
   # ==========================================================================
   'pages': ('Special page names', None, (
-    ('page_front_page', u'LanguageSetup',
-     "Name of the front page. We don't expect you to keep the default. Just read LanguageSetup in case you're wondering... [Unicode]"),
+      ('page_front_page', u'Home',
+     "Name of the front page. We don't expect you to keep the default. [Unicode]"),
 
     # the following regexes should match the complete name when used in free text
     # the group 'all' shall match all, while the group 'key' shall match the key only
