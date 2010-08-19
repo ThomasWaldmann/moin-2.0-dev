9aeffa188d560fc0f801e995a90f409114abb65c
TW <tw@waldmann-edv.de>
2010-08-20 01:40:39 +0200

    add test for +misc/url_names view

--- MoinMoin/apps/misc/_tests/test_misc.py
+++ MoinMoin/apps/misc/_tests/test_misc.py
@@ -16,3 +16,8 @@ class TestMisc(object):
             assert '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' in rv.data
             assert '</urlset>' in rv.data
 
+    def test_urls_names(self):
+        with self.app.test_client() as c:
+            rv = c.get('/+misc/urls_names')
+            assert rv.status == '200 OK'
+            assert rv.headers['Content-Type'] == 'text/plain; charset=utf-8'
