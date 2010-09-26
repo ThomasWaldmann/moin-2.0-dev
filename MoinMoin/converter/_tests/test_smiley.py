74d68b9ff31490b25e5358a3e14e7fa6d8931b1c
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-09-26 19:52:44 +0200

    refactor+cleanup the smiley converter tests to newstyle pytest

--- MoinMoin/converter/_tests/test_smiley.py
+++ MoinMoin/converter/_tests/test_smiley.py
@@ -6,82 +6,73 @@ MoinMoin - Tests for MoinMoin.converter.smiley
 """
 
 import re
-import StringIO
+import py
 
-import py.test
-try:
-    from lxml import etree
-except:
-    py.test.skip("lxml module required to run test for docbook_out converter.")
-import flask
-
-from MoinMoin import log
-logging = log.getLogger(__name__)
-
-from MoinMoin.converter.smiley import *
-
-class Base(object):
-    input_namespaces = ns_all = 'xmlns="%s" xmlns:page="%s"' % (
-        moin_page.namespace,
-        moin_page.namespace,
-    )
-    output_namespaces = {
-        moin_page.namespace: '',
-    }
-
-    namespaces_xpath = {}
-
-    input_re = re.compile(r'^(<[a-z:]+)')
-    output_re = re.compile(r'\s+xmlns="[^"]+"')
-
-    def handle_input(self, input):
-        i = self.input_re.sub(r'\1 ' + self.input_namespaces, input)
-        return ET.XML(i)
-
-    def handle_output(self, elem, **options):
-        file = StringIO.StringIO()
-        tree = ET.ElementTree(elem)
-        tree.write(file, namespaces=self.output_namespaces, **options)
-        return self.output_re.sub(u'', file.getvalue())
-
-    def do(self, input, xpath_query, args={}):
-        with self.app.test_client() as c:
-            rv = c.get('/')
-            out = self.conv(self.handle_input(input), **args)
-        after_conversion = self.handle_output(out)
-        logging.debug("After the SMILEY conversion : %s" % after_conversion)
-        tree = etree.parse(StringIO.StringIO(after_conversion))
-        assert (tree.xpath(xpath_query, namespaces=self.namespaces_xpath))
-
-class TestConverter(Base):
-    def setup_class(self):
-        self.conv = Converter()
-        self.app = flask.Flask('MoinMoin')
-
-    def test_base(self):
-        data = [
-            ('<page><body><p>bla bla :-) bla bla</p></body></page>',
-              '/page/body/p/span[@class="moin-smile"]'),
-            ('<page><body><code>bla bla :-) bla bla</code></body></page>',
-             '/page/body[code="bla bla :-) bla bla"]'),
-            ('<page><body><p>:-) :-(</p></body></page>',
-             '/page/body/p[span[1][@class="moin-smile"]][span[2][@class="moin-sad"]]'),
-            ('<page><body><p><strong>:-)</strong></p></body></page>',
-             '/page/body/p/strong/span[@class="moin-smile"]'),
-            # Test to check we do not have bug with newline in the string
-            ('<page><body><p>1\n2\n3\n4</p></body></page>',
-             '/page/body[p="1\n2\n3\n4"]'),
-            # Test with space between the elements
-            ('<page><body><table-of-content />     <p>text</p></body></page>',
-             '/page/body[p="text"]'),
-            # Test the ignored tags
-            ('<page><body><p><code>:-)</code></p></body></page>',
-             '/page/body/p[code=":-)"]'),
-            # Test the ignored tags and subelement
-            ('<page><body><blockcode>:-)<strong>:-(</strong></blockcode></body></page>',
-              '/page/body/blockcode[text()=":-)"][strong=":-("]'),
-           ]
-        for i in data:
-            yield (self.do, ) + i
+from MoinMoin.converter.smiley import Converter, moin_page, ET
 
 
+def pytest_generate_tests(metafunc):
+    for name, (input, query) in sorted(test_data.items()):
+        metafunc.addcall(id=name, funcargs={'input': input, 'query': query})
+
+output_namespaces = {
+    moin_page.namespace: '',
+}
+
+input_re = re.compile(r'^(<[a-z:]+)')
+output_re = re.compile(r'\s+xmlns="[^"]+"')
+
+
+test_data = {
+    'normal': (
+        '<page><body><p>bla bla :-) bla bla</p></body></page>',
+        '/page/body/p/span[@class="moin-smile"]'),
+    'in code': (
+        '<page><body><code>bla bla :-) bla bla</code></body></page>',
+        '/page/body[code="bla bla :-) bla bla"]'),
+    '2 at once': (
+        '<page><body><p>:-) :-(</p></body></page>',
+        '/page/body/p'
+            '[span[1][@class="moin-smile"]]'
+            '[span[2][@class="moin-sad"]]'),
+    'strong': (
+        '<page><body><p><strong>:-)</strong></p></body></page>',
+        '/page/body/p/strong/span[@class="moin-smile"]'),
+    # Test to check we do not have bug with newline in the string
+    'ok with newlines': (
+        '<page><body><p>1\n2\n3\n4</p></body></page>',
+        '/page/body[p="1\n2\n3\n4"]'),
+    # Test with space between the elements
+    'space between elem': (
+        '<page><body><table-of-content />     <p>text</p></body></page>',
+        '/page/body[p="text"]'),
+    # Test the ignored tags
+    'ignored tag': (
+        '<page><body><p><code>:-)</code></p></body></page>',
+        '/page/body/p[code=":-)"]'),
+    # Test the ignored tags and subelement
+    'ignored tag and subelem': (
+        '<page><body><blockcode>:-)<strong>:-(</strong>'
+        '</blockcode></body></page>',
+        '/page/body/blockcode[text()=":-)"][strong=":-("]'),
+}
+
+
+def ET_to_string(elem, **options):
+    data = []
+    elem.write(data.append, namespaces=output_namespaces)
+    return output_re.sub(u'', ''.join(data))
+
+
+def test_smiley_convert(input, query):
+    etree = py.test.importorskip('lxml.etree')
+    conv = Converter()
+    print 'input:', input
+    out_elem = conv(ET.XML(input))
+    after_conversion = ET_to_string(out_elem)
+    print 'output:', after_conversion
+    print 'query:', query
+    tree = etree.fromstring(after_conversion)
+    result = tree.xpath(query)
+    print 'query result:', result
+    assert result
