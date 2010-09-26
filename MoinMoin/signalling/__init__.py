f2c141ed63a1a6052c04997cbc7c4768c2767779
TW <tw@waldmann-edv.de>
2010-09-27 01:00:11 +0200

    restructure signalling package

--- MoinMoin/signalling/__init__.py
+++ MoinMoin/signalling/__init__.py
@@ -9,24 +9,9 @@
     @license: GNU GPL, see COPYING for details.
 """
 
-from blinker import Namespace, ANY
+# import all signals so they can be imported from here:
+from signals import *
 
-_signals = Namespace()
+# import all signal handler modules so they install their handlers:
+import log
 
-item_displayed = _signals.signal('item_displayed')
-item_modified = _signals.signal('item_modified')
-
-
-from MoinMoin import log
-logging = log.getLogger(__name__)
-
-
-@item_displayed.connect_via(ANY)
-def log_item_displayed(app, item_name):
-    wiki_name = app.cfg.interwikiname
-    logging.info("item %s:%s displayed" % (wiki_name, item_name))
-
-@item_modified.connect_via(ANY)
-def log_item_modified(app, item_name):
-    wiki_name = app.cfg.interwikiname
-    logging.info("item %s:%s modified" % (wiki_name, item_name))
