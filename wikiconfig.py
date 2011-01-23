3dcafc4174b42d93eece36bd19033f47d318118a
TW <tw@waldmann-edv.de>
2011-01-23 23:02:38 +0100

    wikiconfig: serve svg-edit from env/ dir

--- wikiconfig.py
+++ wikiconfig.py
@@ -57,9 +57,10 @@ class Config(DefaultConfig):
         jquery = '/usr/share/javascript/jquery', # debian/ubuntu jquery location
         # Download at http://sourceforge.net/projects/anywikidraw/ - 0.14 works
         anywikidraw = os.path.join(wikiconfig_dir, 'env', 'AnyWikiDraw', 'anywikidraw', 'moinmoin'),
-        # Download at http://static.moinmo.in/files/packages/TWikiDrawPlugin-moin.tar.gz:
+        # Download at http://static.moinmo.in/files/packages/TWikiDrawPlugin-moin.tar.gz
         twikidraw = os.path.join(wikiconfig_dir, 'env', 'TWikiDrawPlugin'),
-        svgedit = os.path.join(wikiconfig_dir, 'MoinMoin', 'static', 'applets', 'svg-edit'),
+        # Download at http://static.moinmo.in/files/packages/svg-edit.tar.gz
+        svgedit = os.path.join(wikiconfig_dir, 'env', 'svg-edit'),
     )
 
     # ^^^ DON'T TOUCH THIS EXCEPT IF YOU KNOW WHAT YOU DO ^^^
