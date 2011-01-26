9e8bd1b20b9e7aee1383931399a74f10a9baa821
TW <tw@waldmann-edv.de>
2011-01-26 02:50:50 +0100

    remove unused emit_custom_html() method

--- MoinMoin/themes/__init__.py
+++ MoinMoin/themes/__init__.py
@@ -96,18 +96,6 @@ class ThemeSupport(object):
             return item_lang_default
         return item_en
 
-    def emit_custom_html(self, html):
-        """
-        Generate custom HTML code in `html`
-        @param html: a string or a callable object, in which case
-                 it is called and its return value is used
-        @rtype: string
-        @return: string with html
-        """
-        if html and callable(html):
-            html = html()
-        return html
-
     def location_breadcrumbs(self, item_name):
         """
         Assemble the location using breadcrumbs (was: title)
